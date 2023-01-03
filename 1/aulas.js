64f2f73553f2b3e930b58134b384720f4120fa0b
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-01-03 16:11:21 -0300

    formatando

--- 1/aulas.js
+++ 1/aulas.js
@@ -1,5 +1,11 @@
-/**calculo IMC */
+/**calculo IMC *
 const peso = 82;
 const altura = 1.77;
 imc = peso / (altura * altura);
-console.log(imc);
+console.log(imc);*/
+
+/**usando janelas de prompt *
+const n1 = prompt('Digite um numero');
+const n2 = prompt('Digite um numero');
+const total = parseFloat(n1) + parseFloat (n2);
+alert('O resultado da operação é' + ' ' + total);*/
