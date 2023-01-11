1e31bba1dd24f87aa138c6a66dfe5ce7e6be3c51
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-01-11 14:23:47 -0300

    conclusao exercicio number 2

--- 1/js/exe2.js
+++ 1/js/exe2.js
@@ -6,13 +6,10 @@ const numerotitulo = document.getElementById('numero-titulo');
 const texto = document.getElementById('texto');
 
 numerotitulo.innerHTML = numero;
-texto.innerHTML = "teste"
-texto.innerHTML = "teste"
-texto.innerHTML = "teste"
-texto.innerHTML = "teste55"
-texto.innerHTML =`<p><span style="margin:25px">A raiz quadrada é: <strong> ${Math.sqrt(numero)} </span></p>`;
-texto.innerHTML =`<p><span style="margin:25px"><strong>${numero} é inteiro: ${Number.isInteger(numero)}</span></p>`;
-texto.innerHTML =`<p><span style="margin:25px">E NaN ? <strong>${Number.isNaN(numero)}</strong></span> </p>`;
-texto.innerHTML =`<p><span style="margin:25px">Arredondando para baixo: <strong>${Math.floor(numero)}</strong></span></p>`;
-texto.innerHTML = `<p><span style="margin:25px">Arredondando para cima: <strong>${Math.ceil(numero)}</strong></span></p>`;
-texto.innerHTML = `<p><span style="margin:25px">Com 2 casas decimais: <strong>${numero.toFixed(2)}</strong></span></p>`;
+texto.innerHTML += " "; //zerando o placeloader
+texto.innerHTML +=`<p><span style="margin:25px">A raiz quadrada é: <strong> ${Math.sqrt(numero)} </span></p>`;
+texto.innerHTML +=`<p><span style="margin:25px"><strong>${numero} é inteiro: ${Number.isInteger(numero)}</span></p>`;
+texto.innerHTML +=`<p><span style="margin:25px">E NaN ? <strong>${Number.isNaN(numero)}</strong></span> </p>`;
+texto.innerHTML +=`<p><span style="margin:25px">Arredondando para baixo: <strong>${Math.floor(numero)}</strong></span></p>`;
+texto.innerHTML += `<p><span style="margin:25px">Arredondando para cima: <strong>${Math.ceil(numero)}</strong></span></p>`;
+texto.innerHTML += `<p><span style="margin:25px">Com 2 casas decimais: <strong>${numero.toFixed(2)}</strong></span></p>`;
