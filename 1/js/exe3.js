a095e6916107e9b2f7749a961595690815d039ce
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-01-13 15:39:28 -0300

    exercicio formulario

--- 1/js/exe3.js
+++ 1/js/exe3.js
@@ -1,15 +1,35 @@
 //let numero = prompt('Digite um número: ');
 //numero = Number(numero);//convertendo para number
 //ou
-const numero = Number (prompt('Dgite um numero'));
-const numerotitulo = document.getElementById('numero-titulo');
-const texto = document.getElementById('texto');
 
-numerotitulo.innerHTML = numero;
-texto.innerHTML += " "; //zerando o placeloader
-texto.innerHTML +=`<p><span style="margin:25px">A raiz quadrada é: <strong> ${Math.sqrt(numero)} </span></p>`;
-texto.innerHTML +=`<p><span style="margin:25px"><strong>${numero} é inteiro: ${Number.isInteger(numero)}</span></p>`;
-texto.innerHTML +=`<p><span style="margin:25px">E NaN ? <strong>${Number.isNaN(numero)}</strong></span> </p>`;
-texto.innerHTML +=`<p><span style="margin:25px">Arredondando para baixo: <strong>${Math.floor(numero)}</strong></span></p>`;
-texto.innerHTML += `<p><span style="margin:25px">Arredondando para cima: <strong>${Math.ceil(numero)}</strong></span></p>`;
-texto.innerHTML += `<p><span style="margin:25px">Com 2 casas decimais: <strong>${numero.toFixed(2)}</strong></span></p>`;
+function meuEscopo() {
+    const form = document.querySelector(".form"); //selecionando classe usa- se .
+    const resultado = document.querySelector('#resultado');
+    const pessoas = [];
+
+    //***********função para enviar os elementos pegos no form
+    function recebeEventoForm(evento) {
+        evento.preventDefault();
+
+        const nome = form.querySelector('#nome');//selecionando ids usa - se # .
+        const sobrenome = form.querySelector('#sobrenome');
+        const peso = form.querySelector('#peso');
+        const altura = form.querySelector('#altura');
+
+        pessoas.push({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value,
+        });
+        console.log(pessoas);
+
+        resultado.innerHTML += `<p>${nome.value}, ${sobrenome.value}, ${peso.value}, ${altura.value},</p>`
+    }
+
+    form.addEventListener('submit', recebeEventoForm);
+    //***********fim da função
+}
+
+meuEscopo();
+
