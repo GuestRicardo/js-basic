8239655391033e954b6c3242d44f08ecb4dc8727
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-01-19 15:56:02 -0300

    RESOUVENDO EXERCICIO

--- 1/assets/js/exe5.js
+++ 1/assets/js/exe5.js
@@ -3,16 +3,42 @@ const form = document.querySelector("#form");
 //capturar o evento de submit do formulario
 form.addEventListener('submit', function (event) {
     event.preventDefault();
-    console.log('evento previnido');//para confirmar q esta funcionando
-    setResultado('teste')
+    //capturando o q esta dentro dos inputs
+    const inputPeso = e.target.querySelector('#peso');
+    const inputAltura = e.target.querySelector('#altura');
+
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+    //------fim dos inputs------------
+
+    //para caso passar um valor q nao seja numero
+    if (!peso){
+       // alert('CARACTERE INACEITAVEL, favor usar numeros');
+        setResultad('Peso invalido', false);
+        return;
+    }
+
+    if (!altura){
+       // alert('CARACTERE INACEITAVEL, favor usar numeros');
+        setResultad('Altura invalida', false);
+        return;
+    }
+
+
 });
 
+//criador de paragrafos
+function criaP() {
+    const p = document.createElement('p'); //esta sendo criado um paragrafo pelo js
+    return p;
+}
+
 //função criada para enviar resultado, serve so pra colocar um html dentro de uma div(usando um id)
-function setResultado(mensagem) {
+function setResultado(mensagem, isValid) {
     const resultado = document.querySelector('#resultado');
     resultado.innerHTML = ''; //limpando o resultado
-    const p = document.createElement('p'); //esta sendo criado um paragrafo pelo js
-    p.classList.add('paragrafo-resultado'); //add class no paragrafo, para editar suas propriedades
-    p.innerHTML = 'testando se paragrafo esta funcionando'//adicionando itens no paragrafo
-    resultado.appendChild(p); //adicionando paragrafo no html via id resultado
+
+    const p = criaP();
+    p.innerHTML = mensagem;
+    resultado.appendChild(p);
 }
