dbd3627328d3d4549b89c7a5d6f420791fd067ec
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-02-01 15:43:48 -0300

    FORMATANDO

---
+++ 1/assets/js/exe7.js
@@ -0,0 +1,6 @@
+const elementos = [
+    { tag: 'p', texto: 'frase 1' },
+    { tag: 'div', texto: 'frase 2' },
+    { tag: 'footer', texto: 'frase 3' },
+    { tag: 'section', texto: 'frase 4' },
+];
