3a2479ae048cfc8842536e190496c4fd6e76ee21
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-02-01 16:22:48 -0300

    finalizando exercicio de for

--- 1/assets/js/exe7.js
+++ 1/assets/js/exe7.js
@@ -1,13 +1,19 @@
 const elementos = [
-    { tag: 'p', texto: 'frase 1' },
-    { tag: 'div', texto: 'frase 2' },
-    { tag: 'section', texto: 'frase 3' },
-    { tag: 'footer', texto: 'frase 4' },
+    { tag: 'p', texto: 'Aq esta sendo criado uma tag p' },
+    { tag: 'div', texto: 'Aq esta sendo criado uma div' },
+    { tag: 'section', texto: 'Aq esta sendo criado uma section' },
+    { tag: 'footer', texto: 'Aq esta sendo criado um footer' },
 ];
 
 const container = document.querySelector('.container');// selecionando a base onde vai ficar todas as criações
 const div = document.createElement('div');//aq esta sendo selecionaddo onde vai exibir todas as criações
 
 for (let i = 0; i < elementos.length; i++) {
-    console.log(elementos[i]);
+    //desestruturando os objetos
+    let { tag, texto } = elementos[i]; //a cada interação com o laço sera dicionado um elemento do array
+    let tagCriada = document.createElement(tag); //criando os elementos
+    tagCriada.innerText = texto; //colocando o texto dentro do 'p'
+    div.appendChild(tagCriada);//pegando todos os elementos e adicionando na tag div criada
 }
+
+container.appendChild(div);
