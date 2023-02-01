0a52cb28b44ba982e45f10f8d3a8b6be8162184b
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-02-01 16:00:42 -0300

    formatando

--- 1/assets/js/exe7.js
+++ 1/assets/js/exe7.js
@@ -1,6 +1,13 @@
 const elementos = [
     { tag: 'p', texto: 'frase 1' },
     { tag: 'div', texto: 'frase 2' },
-    { tag: 'footer', texto: 'frase 3' },
-    { tag: 'section', texto: 'frase 4' },
+    { tag: 'section', texto: 'frase 3' },
+    { tag: 'footer', texto: 'frase 4' },
 ];
+
+const container = document.querySelector('.container');// selecionando a base onde vai ficar todas as criações
+const div = document.createElement('div');//aq esta sendo selecionaddo onde vai exibir todas as criações
+
+for (let i = 0; i < elementos.length; i++) {
+    console.log(elementos[i]);
+}
