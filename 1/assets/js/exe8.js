293c2403e2003204129fc117255d42811417b9be
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-02-03 15:05:51 -0300

    formatando

--- 1/assets/js/exe8.js
+++ 1/assets/js/exe8.js
@@ -1,1 +1,12 @@
-const paragrafos = document.querySelector('.paragrafos');
+//selecionando o paragrafo
+const paragrafos = document.querySelectorAll('.paragrafos');
+//selecionando estilo
+const estiloBody = getComputedStyle(document.body);//esta função nao é do node, é do proprio DOM(navegador)
+const backgroundColorBody = estiloBody.backgroundColor;//pegando a cor de fundo do body
+console.log(backgroundColorBody);
+//aplicando cor do body nos paragrafos
+for (let p of paragrafos){
+   p.style.backgroundColor = backgroundColorBody;//aplcando a cor
+   p.style.color= '#FFFFFF';
+}
+
