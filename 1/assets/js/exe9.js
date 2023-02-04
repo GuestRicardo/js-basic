af4456be5377dbc7c926f533596d903402e1b172
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-02-04 14:13:42 -0300

    resouvendo o exercicio

--- 1/assets/js/exe9.js
+++ 1/assets/js/exe9.js
@@ -1,3 +1,16 @@
-const form = document.querySelector(.form);
+const form = document.querySelector("#form");
 
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const inputN1 = event.target.querySelector('#n1');
+    const inputN2 = event.target.querySelector('#n2');
+    const resultado = document.querySelector('#resultado');
+
+    const n1 = Number(inputN1.value);
+    const n2 = Number(inputN2.value);
+    
+    resultado.innerHTML = `<span style="text-align: center;"> ${n1} ${n2}</span>`;
+
+});
 
