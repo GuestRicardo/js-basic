2a9e5e9966fef72c9d5a902ac44cfd5021258544
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-02-04 14:55:03 -0300

    finalizando primeiro exercicio de 3

--- 1/assets/js/exe9.js
+++ 1/assets/js/exe9.js
@@ -6,11 +6,25 @@ form.addEventListener('submit', function (event) {
     const inputN1 = event.target.querySelector('#n1');
     const inputN2 = event.target.querySelector('#n2');
     const resultado = document.querySelector('#resultado');
+    
+    
 
     const n1 = Number(inputN1.value);
     const n2 = Number(inputN2.value);
+
+
+
+    if (n1 === n2) {
+        return resultado.innerHTML = `<span style="text-align: center;"> ${n1} e ${n2} são identicas</span>`;
+    } else if (n1 > n2) {
+        return resultado.innerHTML = `<span style="text-align: center;"> ${n1} é maior que ${n2}</span>`;
+    } else if (n2 > n1) {
+        return resultado.innerHTML = `<span style="text-align: center;"> ${n2} é maior que ${n1}</span>`;
+    } else {
+        return 'ERROR';
+    }
     
-    resultado.innerHTML = `<span style="text-align: center;"> ${n1} ${n2}</span>`;
+    // resultado.innerHTML = `<span style="text-align: center;"> ${n1} ${n2}</span>`;
 
 });
 
