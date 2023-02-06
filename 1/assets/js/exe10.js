412520439b562845d2fc52a66bd40f91f05e51bf
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-02-06 16:05:01 -0300

    formatando

--- 1/assets/js/exe10.js
+++ 1/assets/js/exe10.js
@@ -3,14 +3,14 @@ const form = document.querySelector("#form");
 form.addEventListener('submit', function (event) {
     event.preventDefault();
 
-    total = (n1, n2)=>{
+    total = () => {
         const inputN1 = event.target.querySelector('#n1');
         const inputN2 = event.target.querySelector('#n2');
-        const resultado = document.querySelector('#resultado');    
-        
+        const resultado = document.querySelector('#resultado');
+
         const n1 = Number(inputN1.value);
         const n2 = Number(inputN2.value);
-    
+
         if (n1 === n2) {
             return resultado.innerHTML = `<span style="text-align: center;"> ${n1} e ${n2} são identicas</span>`;
         } else if (n1 > n2) {
@@ -19,7 +19,7 @@ form.addEventListener('submit', function (event) {
             return resultado.innerHTML = `<span style="text-align: center;"> ${n2} é maior que ${n1}</span>`;
         } else {
             return 'ERROR';
-        }    
+        }
     };
 
 });
