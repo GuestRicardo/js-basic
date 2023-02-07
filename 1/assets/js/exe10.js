460a9877d446c411fc554b4943e628ba26754322
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-02-07 14:57:09 -0300

    formatando

--- 1/assets/js/exe10.js
+++ 1/assets/js/exe10.js
@@ -2,7 +2,6 @@ const form = document.querySelector("#form");
 
 form.addEventListener('submit', function (event) {
     event.preventDefault();
-
    
         const inputN1 = event.target.querySelector('#n1');
         const inputN2 = event.target.querySelector('#n2');
@@ -12,11 +11,11 @@ form.addEventListener('submit', function (event) {
         const n2 = Number(inputN2.value);
 
         if (n1 === n2) {
-            return resultado.innerHTML = `<span style="text-align: center;"> Tamanho da imagem Normal</span>`;
+            return resultado.innerHTML = `<span style="text-align: center;">${n1} x ${n2} Tamanho da imagem Normal</span>`;
         } else if (n1 > n2) {
-            return resultado.innerHTML = `<span style="text-align: center;"> Imagem widscreen</span>`;
+            return resultado.innerHTML = `<span style="text-align: center;">${n1} x ${n2} Imagem widscreen</span>`;
         } else if (n2 > n1) {
-            return resultado.innerHTML = `<span style="text-align: center;">Tamanho da imagem Normal</span>`;
+            return resultado.innerHTML = `<span style="text-align: center;">${n1} x ${n2} Tamanho da imagem Normal</span>`;
         } else {
             return 'ERROR';
         }
