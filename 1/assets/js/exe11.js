eb2a8775fa7994f714a93da421a5d62214854cb6
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-02-08 16:23:35 -0300

    formatando

--- 1/assets/js/exe11.js
+++ 1/assets/js/exe11.js
@@ -1,2 +1,13 @@
-
+/** 
+ *  
+ *            try{
+ *                 //É executado quando nao há erros
+ *             } catch(erro){
+ *                 //È executado quando houver erro
+ *             } finally {
+ *                 //Sempre será executada 
+ *             } 
+ * 
+ * 
+*/
 
