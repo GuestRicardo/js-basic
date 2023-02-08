0e20608087ac166fb38e9fb274aba65025933735
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-02-08 16:15:50 -0300

    exercicio

--- 1/assets/js/exe11.js
+++ 1/assets/js/exe11.js
@@ -1,23 +1,2 @@
-const form = document.querySelector("#form");
 
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    const inputN1 = event.target.querySelector('#numero');    
-    const resultado = document.querySelector('#resultado');
-
-    const largura = Number(inputN1.value);
-    
-
-    if (largura === altura) {
-        return resultado.innerHTML = `<p>Sua Imagem esta quadrada</p>`;
-    } else if (largura > altura) {
-        return resultado.innerHTML = `<p>Sua Imagem esta em modo paisagem</p>`;
-    } else if (largura < altura) {
-        return resultado.innerHTML = `<p>Sua Imagem esta em modo retrato</p>`;
-    } else{
-        return resultado.innerHTML=`<p>ERRO, apenas use numeros, não pode usar letras</p>`;
-    }
-
-});
 
