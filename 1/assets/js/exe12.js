09e30833d853b8300b6c4dd5d3652e833242cfe8
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-02-10 15:04:19 -0300

    formatando

--- 1/assets/js/exe12.js
+++ 1/assets/js/exe12.js
@@ -6,4 +6,12 @@ const iniciar = document.querySelector('#iniciar');
 const pausar = document.querySelector('#pausar');
 const zerar = document.querySelector('#zerar');
 
-
+iniciar.addEventListener('click', (evento)=>{
+   
+});
+pausar.addEventListener('click', (evento)=>{
+   
+});
+zerar.addEventListener('click', (evento)=>{
+   
+});
