03545cbd58ab428e3c1fcae28295de8c34db499b
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-02-10 13:50:12 -0300

    formatando

--- 1/assets/js/exe12.js
+++ 1/assets/js/exe12.js
@@ -1,35 +1,6 @@
-/** 
- *             try{
- *                 //É executado quando nao há erros
- *             } catch(erro){
- *                 //È executado quando houver erro
- *             } finally {
- *                 //Sempre será executada, mesmo se executar o erro
- *             }   
-*/
-// função para retornar hora
-
-function retornHora(data) {
-    if (data && !(data instanceof Date)) { //a variavel data é uma instancia da função construtora Date, e para execulta-la é preciso usar o (new) exemplo new Date()
-        throw new TypeError('Esperando instancia de Date');// se a data nao for a instancia da Date, ai aq esta sendo lançado o erro
-    }
-    if (!data) {
-        data = new Date();
-    }
-    return data.toLocaleTimeString('pt-BR', {
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-        hour12: false //para tirar o PM q aparece na frente da hora
-    });
-}
-try{
-    const hora = retornaHora();
-    console.log(hora);
-} catch(erro){
-    //tratar erro
-    console.log(erro);
-} finally{
-    console.log('tenha um bom dia');
-}
+const form = document.querySelector('#form');
+const timer = document.querySelector('#timer');
+const iniciar = document.querySelector('#iniciar');
+const pausar = document.querySelector('#pausar');
+const zerar = document.querySelector('#zerar');
 
