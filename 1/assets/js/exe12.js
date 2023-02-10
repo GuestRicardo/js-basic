512437cc4fedc873d09ff0743b378b91ca4b830c
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-02-10 13:53:52 -0300

    editando

--- 1/assets/js/exe12.js
+++ 1/assets/js/exe12.js
@@ -1,6 +1,9 @@
-const form = document.querySelector('#form');
+
+
 const timer = document.querySelector('#timer');
+/*                 *botões                      */
 const iniciar = document.querySelector('#iniciar');
 const pausar = document.querySelector('#pausar');
 const zerar = document.querySelector('#zerar');
+
 
