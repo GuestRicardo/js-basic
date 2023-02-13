8b6c44a76f48717f8ae4a08363c2009f011e8415
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-02-13 14:04:22 -0300

    formatando

--- 1/assets/js/exe12.js
+++ 1/assets/js/exe12.js
@@ -1,5 +1,9 @@
-function temposengundos(){
-
+function criaHorasDosSegundos(segundos) {
+   const data = new Date(segundos * 1000);
+   return data.toLocaleTimeString('pt-BR',{
+      hour12: false,
+      timeZone: 'GMT', //para meia noite ser zero
+   });
 };
 
 const timer = document.querySelector('#timer');
@@ -8,13 +12,13 @@ const iniciar = document.querySelector('#iniciar');
 const pausar = document.querySelector('#pausar');
 const zerar = document.querySelector('#zerar');
 
-iniciar.addEventListener('click', (event)=>{
-   timer.innerHTML ='teste';
+iniciar.addEventListener('click', (event) => {
+   timer.innerHTML = 'teste';
 });
-pausar.addEventListener('click', (event)=>{
-   
+pausar.addEventListener('click', (event) => {
+
 });
-zerar.addEventListener('click', (event)=>{
-   
+zerar.addEventListener('click', (event) => {
+
 });
 
