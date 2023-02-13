2eb54fe6ea51a0258c9366e3d74422ad11afb062
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-02-13 14:47:10 -0300

    formatando

--- 1/assets/js/exe12.js
+++ 1/assets/js/exe12.js
@@ -20,12 +20,14 @@ function iniciaRelogio() {
       relogio.innerHTML = criaHorasDosSegundos(segundos);
    }, 1000);
 }
-
+//** sem a fatoração */
 iniciar.addEventListener('click', (event) => {
+   relogio.classList.remove('pausado');
    clearInterval(timer);
    iniciaRelogio();
 });
 pausar.addEventListener('click', (event) => {
+   relogio.classList.add('pausado');
    clearInterval(timer);
 });
 zerar.addEventListener('click', (event) => {
@@ -33,4 +35,8 @@ zerar.addEventListener('click', (event) => {
    relogio.innerHTML = '00:00:00';
    segundos = 0;
 });
+// fim
 
+/**fatorando os clicks nos botoes */
+
+//fim
