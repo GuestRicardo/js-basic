55c8b9aa302768c6a4eef429a385a096ba03f45b
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-02-13 19:10:41 -0300

    formatando

--- 1/assets/js/exe13.js
+++ 1/assets/js/exe13.js
@@ -1,5 +1,16 @@
 function tarefas() {
-   
+    const inputTarefa = document.querySelector('.input-nova-tarefa');
+    const btnTarefas = document.querySelector('.btn-add-tarefas');
+    const listaTarefas = document.querySelector('.tarefas');
 
+    //capturando o click no btn
+    btnTarefas.addEventListener('click', function(evento){
+        if(!inputTarefa.value) return;//evitando enviar algo vazio               
+        criaTarefa(inputTarefa.value);
+    });
+
+    function criaTarefa(textoInput){  
+        
+    }
 };
 tarefas();
