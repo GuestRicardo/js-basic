ad5d9cb383e38166f450ace90dfb3ece4ddd1583
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-02-13 19:37:17 -0300

    formatando

--- 1/assets/js/exe13.js
+++ 1/assets/js/exe13.js
@@ -4,13 +4,33 @@ function tarefas() {
     const listaTarefas = document.querySelector('.tarefas');
 
     //capturando o click no btn
-    btnTarefas.addEventListener('click', function(evento){
-        if(!inputTarefa.value) return;//evitando enviar algo vazio               
+    btnTarefas.addEventListener('click', function (evento) {
+        if (!inputTarefa.value) return;//evitando enviar algo vazio               
         criaTarefa(inputTarefa.value);
     });
 
-    function criaTarefa(textoInput){  
-        
+    function criaLinha() {
+        const li = document.createElement('li');
+        return li;
+    }
+
+    //pegando ação da tecla enter
+    inputTarefa.addEventListener('keypress', function (evento) {
+        if (evento.keyCode===13){
+            if (!inputTarefa.value) return;
+        }
+    });
+    //fim
+    function criaTarefa(textoInput) {
+        const li = criaLinha();
+        li.innerText = textoInput;
+        listaTarefas.appendChild(li);
+        limpaImput();
+    }
+    //limpando input
+    function limpaImput(){
+        inputTarefa.value= '';
+        inputTarefa.focus();//o cursor fica piscando
     }
 };
 tarefas();
