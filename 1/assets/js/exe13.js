acb1016f792c03bc55722e6e40f54a824895a7b4
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-02-14 10:08:55 -0300

    formatando

--- 1/assets/js/exe13.js
+++ 1/assets/js/exe13.js
@@ -2,35 +2,38 @@ function tarefas() {
     const inputTarefa = document.querySelector('.input-nova-tarefa');
     const btnTarefas = document.querySelector('.btn-add-tarefas');
     const listaTarefas = document.querySelector('.tarefas');
-
-    //capturando o click no btn
-    btnTarefas.addEventListener('click', function (evento) {
-        if (!inputTarefa.value) return;//evitando enviar algo vazio               
-        criaTarefa(inputTarefa.value);
-    });
-
+    
     function criaLinha() {
         const li = document.createElement('li');
         return li;
     }
-
+    
     //pegando ação da tecla enter
     inputTarefa.addEventListener('keypress', function (evento) {
         if (evento.keyCode===13){
             if (!inputTarefa.value) return;
+            criaTarefa(inputTarefa.value);
         }
     });
     //fim
+
+    //limpando input
+    function limpaImput(){
+        inputTarefa.value= '';
+        inputTarefa.focus();//o cursor fica piscando
+    }
+    
     function criaTarefa(textoInput) {
         const li = criaLinha();
         li.innerText = textoInput;
         listaTarefas.appendChild(li);
         limpaImput();
     }
-    //limpando input
-    function limpaImput(){
-        inputTarefa.value= '';
-        inputTarefa.focus();//o cursor fica piscando
-    }
+    
+        //capturando o click no btn
+        btnTarefas.addEventListener('click', function (evento) {
+            if (!inputTarefa.value) return;//evitando enviar algo vazio               
+            criaTarefa(inputTarefa.value);
+        });
 };
 tarefas();
