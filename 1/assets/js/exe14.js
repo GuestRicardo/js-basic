047f2434eaf5002df494b9a0ac62645d3290cb08
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-02-23 16:47:38 -0300

    formatando

--- 1/assets/js/exe14.js
+++ 1/assets/js/exe14.js
@@ -1,6 +1,13 @@
 //sera usando uma factory function, entao ela retornara um objeto
 function criaCalculadora() {
     return {
+        //atributos
+        display:document.querySelector('.display'),
+        
+
+
+
+        //metodos
         inicia() { //esse metodo vai descandear todos os outros abaixo
             alert('teste');
         },
