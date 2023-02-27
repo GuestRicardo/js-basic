386ac9200b1cee37d0a1e75e2f2224830af90a9f
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-02-27 14:30:15 -0300

    formatando

--- 1/assets/js/exe15.js
+++ 1/assets/js/exe15.js
@@ -1,78 +1,75 @@
 //sera usando uma factory function, entao ela retornara um objeto
-function criaCalculadora() {
-    return {
-        //atributos
-        display: document.querySelector('.display'),
-        //btnClear: document.querySelector('.btn-clear'),
-        //btnDel: document.querySelector('.btn-del'),
-        //btnIgual: document.querySelector('.btn-eq'),
+function Calculadora() {
+    //atributos
+    display: document.querySelector('.display');
+    //btnClear: document.querySelector('.btn-clear'),
+    //btnDel: document.querySelector('.btn-del'),
+    //btnIgual: document.querySelector('.btn-eq'),
 
 
-        //metodos
-        inicia() { //esse metodo vai descandear todos os outros abaixo
-            //alert('teste');
-            this.clickBotoes();
-            this.pressionaEnter();
-        },
+    //metodos
+    this.inicia() { //esse metodo vai descandear todos os outros abaixo
+        //alert('teste');
+        this.clickBotoes();
+        this.pressionaEnter();
+    };
 
-        pressionaEnter(){
-           this.display.addEventListener('keyup', evento =>{
-                if(evento.keyCode === 13){
-                    this.realizaConta();
-                }
-           }); 
-        },
+    this.pressionaEnter(){
+        this.display.addEventListener('keyup', evento => {
+            if (evento.keyCode === 13) {
+                this.realizaConta();
+            }
+        });
+    };
 
-        realizaConta() {
-            let calculo = this.display.value;
+    this.realizaConta() {
+        let calculo = this.display.value;
 
-            try {
-                calculo = eval(calculo);
-                if (!calculo) {
-                    alert('Conta Invalida');
-                    return;
-                }
-                this.display.value = String(calculo);
-            } catch (e) {
+        try {
+            calculo = eval(calculo);
+            if (!calculo) {
                 alert('Conta Invalida');
                 return;
             }
+            this.display.value = String(calculo);
+        } catch (e) {
+            alert('Conta Invalida');
+            return;
+        }
 
-        },
-
-        clearDsiplay() {
-            this.display.value = '';
-        },
-
-        btnDelete() {
-            this.display.value = this.display.value.slice(0, -1);
-        },
-
-
-
-        clickBotoes() {
-            document.addEventListener('click', evento => { //a arrow function aq e a mais indicada, pois isola o this, somente para ela
-                const elemento = evento.target;
-
-                if (elemento.classList.contains('btn-num')) {
-                    this.btnParaDisplay(elemento.innerText);
-                }
-                if (elemento.classList.contains('btn-clear')) {
-                    this.clearDsiplay();
-                }
-                if (elemento.classList.contains('btn-del')) {
-                    this.btnDelete();
-                }
-                if (elemento.classList.contains('btn-eq')) {
-                    this.realizaConta();
-                }
-            });
-        },
-
-        btnParaDisplay(valor) {
-            this.display.value += valor;
-        },
     };
-}
-const calculadora = criaCalculadora();
+
+    this.clearDsiplay() {
+        this.display.value = '';
+    };
+
+    this.btnDelete() {
+        this.display.value = this.display.value.slice(0, -1);
+    };
+
+    this.clickBotoes() {
+        document.addEventListener('click', evento => { //a arrow function aq e a mais indicada, pois isola o this, somente para ela
+            const elemento = evento.target;
+
+            if (elemento.classList.contains('btn-num')) {
+                this.btnParaDisplay(elemento.innerText);
+            }
+            if (elemento.classList.contains('btn-clear')) {
+                this.clearDsiplay();
+            }
+            if (elemento.classList.contains('btn-del')) {
+                this.btnDelete();
+            }
+            if (elemento.classList.contains('btn-eq')) {
+                this.realizaConta();
+            }
+        });
+    };
+
+    this.btnParaDisplay(valor) {
+        this.display.value += valor;
+    };
+
+};
+const calculadora = new Calculadora();
 calculadora.inicia();
