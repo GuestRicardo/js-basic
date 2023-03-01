0dcba8579756c2d90ebf8262050a74f615f0cf18
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-03-01 10:25:46 -0300

    formatando

---
+++ 1/assets/js/exe16.js
@@ -0,0 +1,15 @@
+//retorne numeros maiores que 10
+const numeros = [12, 3, 55, 384, 484, 5, 9874];
+
+//Retorne as pessoas que tem o nome com mais de 5 letras ou mais
+//Retorne as pessoas com mais de 50 anos
+//Retorne as pessoas cujo os nomes terminam com a letra A
+
+const pessoas = [
+    { nome: 'Maria', idade: 61 },
+    { nome: 'Moises', idade: 21 },
+    { nome: 'Monica', idade: 55 },
+    { nome: 'Leticia', idade: 18 },
+    { nome: 'Walace', idade: 31 },
+    { nome: 'Rosana', idade: 47 },
+];
