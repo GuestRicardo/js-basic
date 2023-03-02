4c6514cd10c8efc9c7be31147337d4c778892096
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-03-02 20:56:00 -0300

    formatando

--- 1/assets/js/exe18.js
+++ 1/assets/js/exe18.js
@@ -11,6 +11,7 @@ console.log(`Retornando a soma de todos os numeros é ${somaNumeros}`);
 console.log('');
 /****************************************************************************** */
 //retorne os arrays com valores pares
+//nesse exemplo o ideal é usar o FILTER, pois o exemplo abaixo é so pra entender o reduce
 const numerosPar = numeros.reduce(function (acumulador, valor, indice, array) {
     if (valor % 2 === 0) {
         acumulador.push(valor);
@@ -20,36 +21,24 @@ const numerosPar = numeros.reduce(function (acumulador, valor, indice, array) {
 //numeros pares
 console.log(`Retornando valores Pares ${numerosPar}`);
 console.log('');
-
-/** *
+/************************************************************************************** */
 //lista de pessoas
 const pessoas = [
-    { nome: 'Maria', idade: 61 },
-    { nome: 'Moises', idade: 21 },
-    { nome: 'Monica', idade: 55 },
-    { nome: 'Leticia', idade: 18 },
-    { nome: 'Walace', idade: 31 },
-    { nome: 'Rosana', idade: 47 },
-    { nome: 'Lenon', idade: 4 },
-    { nome: 'Leo', idade: 14 },
-    { nome: 'Bidu', idade: 14 },
-    { nome: 'Biu', idade: 24 },
+    { nome: 'Maria', idade: 61},
+    { nome: 'Moises', idade: 21},
+    { nome: 'Monica', idade: 55},
+    { nome: 'Leticia', idade: 18},
+    { nome: 'Walace',idade: 31},
+    { nome: 'Rosana', idade: 87},
+    { nome: 'Lenon', idade: 4},
+    { nome: 'Leo', idade: 14},
+    { nome: 'Bidu', idade: 17},
+    { nome: 'Biu', idade: 24},
 ];
-//Retorne apenas uma string com nome da pessoa
-const somenteNome = pessoas.map(valor => valor.nome);
-//Remova apenas a chave nome do objeto
-const sementeIdade = pessoas.filter(valor => ({ idade: valor.idade }));
-//Adicione uma chave id em cada objeto
-const addumIndice = pessoas.filter((valor, indice) => {
-    //valor.id = (indice +1) * 100; //para nao ter o id 0
-
-    //para nao mexer no objeto original, é preciso criar uma copia
-    const copiaObj = { ...valor };
-    copiaObj.id = indice;
-    return copiaObj;
+//Retorne a pessoa mais velha
+const pessoaMaisVelha = pessoas.reduce((acumulador, valor)=>{
+    if (acumulador.idade > valor.idade) return acumulador; //neste exemplo quando retorna o acumulador esta sendo retornado o objeto inteiro
+    return valor;    
 });
 
-console.log(somenteNome);
-console.log(somenteIdade);
-console.log(addumIndice);
-*/
+console.log(`A pessoa mais velha da lista é ${pessoaMaisVelha}`);
