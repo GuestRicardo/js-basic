b6c4d182b05ae4988b4f7f4e609202a9cae1d065
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-03-06 15:12:57 -0300

    formatando

--- 1/assets/js/exe19.js
+++ 1/assets/js/exe19.js
@@ -1,13 +1,20 @@
 // retorne a soma do dobro de todos os pares(map) 
 // filtrar os pares (filter)
-// dobrar os valores
-// reduzir(somar todos) 
+// dobrar os valores(map)
+// somar todos (reduce) 
 const numeros = [12, 3, 55, 384, 484, 5, 74];
 
 // retorne os numeros pares(filter)
 const numerosPares = numeros.filter((valor)=>{
     return valor % 2 === 0;
 });
+const dobro = numerosPares.map((valor)=>{ //dobrando os numeros pares
+    return valor * 2;
+});
+const somaTotal = dobro.reduce((acumulador, valor)=>{//somando todos os resultados(reduce)
+    return acumulador + valor;
+}) 
 
 
-console.log(`os numeros pares são ${numerosPares}`);
+console.log(`os numeros pares são ${numerosPares} e o dobro desses numeros é ${dobro}`);
+console.log(`A soma de todos os valores pares em dobro ${somaTotal}`);
