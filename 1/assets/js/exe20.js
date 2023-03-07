e0d7b56d6b66f56437d5907b600651672a894c60
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-03-07 15:40:27 -0300

    formatando

--- 1/assets/js/exe20.js
+++ 1/assets/js/exe20.js
@@ -1,6 +1,11 @@
 //Objeto literal
-const pessoa ={//corpo do objeto
+const pessoas = { //corpo do objeto
     //chaves para acesso ao objeto
     nome: 'Maria',
     sobrenome: 'Joaquina'
 };
+//formas de acesso as chaves do objeto(anotação de ponto e colchetes)
+//por ponto(é a forma mais indicada)
+console.log(pessoas.nome);
+//mas tambem da pra acessar por colchetes(ela é mais interessante usar no caso de valores dinamicos )
+console.log(pessoas['sobrenome']);
