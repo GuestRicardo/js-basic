31fbcfd74ee0c962d128601b9dae1a157d375867
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-03-10 16:35:34 -0300

    formatando

--- 1/assets/js/exe21.js
+++ 1/assets/js/exe21.js
@@ -6,8 +6,10 @@ function Produto(nome, preco, estoque) {
     this.estoque = estoque;
 
     Object.defineProperty(this, 'estoque', { //o this esta indicando o objeto q esta sendo criado, por isso q ele sera selecionado
-        enumerable: true,
-        value: estoque
+        enumerable: true, //mostra a propriedade, mas se tiver como false é o contrario
+        value: estoque, //valor da chave
+        writable: false, //para q o valor nao seja alterado, mas se tiver como fase é ao contrario  
+        configurable: true, //aq é se aceita apagar a chave, da mesma forma q os outros se caso estiver false vai aceitar ao contrario 
     });
 
 };
