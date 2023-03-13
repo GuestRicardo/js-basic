d5de561544c50212636f9ab9a727da12bc2828a1
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-03-13 15:55:48 -0300

    formatando

--- 1/assets/js/exe23.js
+++ 1/assets/js/exe23.js
@@ -4,6 +4,8 @@ function Produto(nome, preco, estoque) {
     this.nome = nome; //o this torna a variavbel publica
     this.preco = preco;
 
+    let estoquePrivado = estoque;
+
     Object.defineProperty (this, 'estoque', {
         nome: {
             enumerable: true, //mostrar chave
@@ -11,14 +13,22 @@ function Produto(nome, preco, estoque) {
             //o gett so exibe o valor
             get: function(){
                 return estoque;
+            },
+            set: function(valor){
+                if(typeof valor !== 'number'){
+                    console.log('estoque so aceita numeos')
+                }
+                estoquePrivado = valor;
             }
         },
 
     });
 
 };
-
 const produto1 = new Produto('Blusa', 21, 3);
+//adicionando estoque
+produto1 = 522;
+
 console.log(produto1.estoque);
 
 for(let chave in produto1){
