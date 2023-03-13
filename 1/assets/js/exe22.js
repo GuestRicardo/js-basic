19efa4dabe2005fae6d7fdc6dd22165e2ba3f742
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-03-13 13:44:12 -0300

    formatando

--- 1/assets/js/exe22.js
+++ 1/assets/js/exe22.js
@@ -2,20 +2,20 @@
 //neste exemplo ele sera edxibido para ser usado por outras funçoes  
 function Produto(nome, preco, estoque) {
     this.nome = nome; //o this torna a variavbel publica
-    this.preco = preco;    
+    this.preco = preco;
 
-    Object.defineProperties(this, { 
-       nome: {
-        enumerable: true,
-        value: nome,
-        writable: false,
-        configurable: true,
-       },
+    Object.defineProperties(this, {
+        nome: {
+            enumerable: true,
+            value: nome,
+            writable: false,
+            configurable: true,
+        },
 
         preco: {
-            enumerable: true, 
-            value: preco, 
-            writable: false,  
+            enumerable: true,
+            value: preco,
+            writable: false,
             configurable: true,
         },
     });
