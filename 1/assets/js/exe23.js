f52d7b9cd90e88791d986ed54e693a7f02f8e5cd
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-03-13 15:37:03 -0300

    formatando

--- 1/assets/js/exe23.js
+++ 1/assets/js/exe23.js
@@ -4,19 +4,12 @@ function Produto(nome, preco, estoque) {
     this.nome = nome; //o this torna a variavbel publica
     this.preco = preco;
 
-    Object.defineProperties(this, {
+    Object.defineProperty (this, 'estoque', {
         nome: {
             enumerable: true,
-            value: nome,
-            writable: false,
             configurable: true,
         },
-        preco: {
-            enumerable: true,
-            value: preco,
-            writable: false,
-            configurable: true,
-        },
+
     });
 
 };
