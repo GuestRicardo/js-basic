1578b92ef107049deeb1fe4e8d821a2138c4cde3
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-03-13 15:28:25 -0300

    formatando

---
+++ 1/assets/js/exe23.js
@@ -0,0 +1,29 @@
+//função construtora
+//neste exemplo ele sera edxibido para ser usado por outras funçoes  
+function Produto(nome, preco, estoque) {
+    this.nome = nome; //o this torna a variavbel publica
+    this.preco = preco;
+
+    Object.defineProperties(this, {
+        nome: {
+            enumerable: true,
+            value: nome,
+            writable: false,
+            configurable: true,
+        },
+        preco: {
+            enumerable: true,
+            value: preco,
+            writable: false,
+            configurable: true,
+        },
+    });
+
+};
+
+const produto1 = new Produto('Blusa', 21,3);
+console.log(produto1);
+
+for(let chave in produto1){
+    console.log(chave);
+}
