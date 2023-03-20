01416d12539af165cdea4fd2d16e8393114c8649
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-03-20 15:43:25 -0300

    formatando

--- 1/assets/js/exe24.js
+++ 1/assets/js/exe24.js
@@ -1,8 +1,15 @@
 //para add o cpf
-function ValidaCpf(cpfEnviado){
-Object.defineProperty(this, 'cpfLimpo',{
-    get: function (){
-        return cpfEnviado.replace(/\D+/, ''); //para tirar os pontos e sinais do cpf
-    }
-})
+function ValidaCpf(cpfEnviado) {
+    Object.defineProperty(this, 'cpfLimpo', {
+        get: function () {
+            return cpfEnviado.replace(/\D+/, ''); //para tirar os pontos e sinais do cpf
+        }
+    });
 }
+//para validar o cpf
+ValidaCpf.prototype.valida = function () {
+    if (typeof cpfLimpo === 'undefined') {
+        return false;
+    }
+    return true;
+};
