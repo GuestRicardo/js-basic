50d4b0b29ae967479de015279819a0985bc262c2
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-03-20 16:11:42 -0300

    formatando

--- 1/assets/js/exe24.js
+++ 1/assets/js/exe24.js
@@ -2,20 +2,21 @@
 function ValidaCpf(cpfEnviado) {
     Object.defineProperty(this, 'cpfLimpo', {
         get: function () {
-            return cpfEnviado.replace(/\D+/, ''); //para tirar os pontos e sinais do cpf
+            return cpfEnviado.replace(/\D+/g, ''); //para tirar os pontos e sinais do cpf
         }
     });
 }
 //para validar o cpf
 ValidaCpf.prototype.valida = function () {
     if (typeof this.cpfLimpo === 'undefined') return false; //caracteres invalidos
-    if (typeof this.cpfLimpo.length !== 11) return false; //se nao mandar os 11 digitos
+    if (this.cpfLimpo.length !== 11) return false; //se nao mandar os 11 digitos
+
     const cpfParcial = this.cpfLimpo.slice(0, -2); //variavel para receber o parametro da função criaDigito
-    this.criaDigito();
+    const digito1 = this.criaDigito(cpfParcial);
     return true;
 };
 
-Valida.prototype.criaDigito = function(cpfParcial){
+ValidaCpf.prototype.criaDigito = function (cpfParcial) {
     //convertendo a string em um array
     const cpfArray = Array.from(cpfParcial);
     console.log(cpfArray);
