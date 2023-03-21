6db414661484edb20dd3c96f617324a9ae423be9
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-03-21 16:22:41 -0300

    formatando

--- 1/assets/js/exe25.js
+++ 1/assets/js/exe25.js
@@ -4,12 +4,29 @@ function Conta(agencia, saldo, conta) {
     this.saldo = saldo;
 };
 
-Conta.prototype.sacar = function(valor){
-    if(this.saldo < valor){
+Conta.prototype.sacar = function (valor) {
+    if (this.saldo < valor) {
         this.verSaldo();
         return;
     }
     this.saldo -= valor;
+    this.verSaldo();
 };
-Conta.prototype.sacar = function(valor){};
-Conta.prototype.sacar = function(){};
+Conta.prototype.depositar = function (valor) {
+    this.saldo += valor;
+    this.verSaldo();
+};
+Conta.prototype.verSaldo = function () {
+    console.log(`Ag/C: ${this.agencia}/ ${this.conta}` + `
+    Saldo: ${this.saldo.toFixed(2)}`);
+};
+
+//criando conta
+const conta1 = new Conta(11, 55, 125);
+//saldo anterior
+console.log()
+//depositando
+conta1.depositar(10);
+//sacando
+conta1.sacar(25);
+console.log()
