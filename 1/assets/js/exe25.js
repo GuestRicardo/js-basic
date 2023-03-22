54467c2845d6f1b84d8d963b4993f8f0594f5776
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-03-22 15:18:08 -0300

    formatando

--- 1/assets/js/exe25.js
+++ 1/assets/js/exe25.js
@@ -18,8 +18,10 @@ Conta.prototype.depositar = function (valor) {
     this.verSaldo();
 };
 Conta.prototype.verSaldo = function () {
-    console.log(`Ag/C: ${this.agencia}/ ${this.conta}` + 
-    `Saldo: ${this.saldo.toFixed(2)}`);
+    console.log(
+    `Ag/C: ${this.agencia}/ ${this.conta}` + 
+    `Saldo: ${this.saldo.toFixed(2)}`
+   );
 };
 
 //conta corrente
