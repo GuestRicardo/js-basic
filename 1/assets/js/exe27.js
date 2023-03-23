313f831318fa3e18892032c2cba1ad3a9aa11769
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-03-23 15:53:18 -0300

    formatando

--- 1/assets/js/exe27.js
+++ 1/assets/js/exe27.js
@@ -1,5 +1,28 @@
-const pessoa = [
-     {id:3, nome: 'Jose'},
-     {id:9, nome: 'Maria'},
-     {id:48, nome: 'Leticia'},
+const pessoa = [{
+        id: 3,
+        nome: 'Jose'
+    },
+    {
+        id: 9,
+        nome: 'Maria'
+    },
+    {
+        id: 48,
+        nome: 'Leticia'
+    },
 ];
+
+const novaPessoa = {};
+//usando destructor
+for (const {id, nome} of pessoas) {
+    console.log(id, nome)
+}
+//adicionando id dentro do novo objeto usando destructor
+for (const {id, nome} of pessoas) {
+    novasPessoas[id] = {id, nome};
+}
+//aq esta sendo atribuido o id individualmente para as pessoas
+for (const pessoa of pessoas) {
+    const { id } = pessoa;
+    novasPessoas[id] = {...pessoa};
+}
