aaeeff5c607879161a58ce89720e5a44fcc93705
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-03-27 16:10:16 -0300

    formatando

--- 1/assets/js/exe28.js
+++ 1/assets/js/exe28.js
@@ -1,15 +1,19 @@
 class Carro {
     constructor(nome) {
         this.nome = nome;
-        this.velocidade = 0; 
+        this.velocidade = 0;
     }
 
-    acelerar(){
-        if(this.velocidade >= 100) return;
+    acelerar() {
+        if (this.velocidade >= 100) return;
         this.velocidade++;
     }
-    freiar(){
-        if(this.velocidade <= 0) return;
-        this.velocidade--;    
+    freiar() {
+        if (this.velocidade <= 0) return;
+        this.velocidade--;
     }
 };
+
+const carro1 = new Carro('Fusca');
+
+//simulando se tivesse acelerando o fusca ate 200km por hra
