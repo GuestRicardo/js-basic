809492692e8a9f22432402970332c9fdeebba5aa
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-03-27 16:37:00 -0300

    formatando

--- 1/assets/js/exe28.js
+++ 1/assets/js/exe28.js
@@ -1,18 +1,19 @@
-//criando uma chave privada dentro do objeto
+//criando uma chave privada dentro do objeto, para evitar de qualquer um tenha acesso a velocidade, no caso mexer no objeto
+//mesmo atribuindo uma velocidade 1000km nao sera mexido na velocidade interna q esta salva no objeto da classe
 const _velocidade = Symbol('velocidade') //valor entre aspas é so a descrição
 class Carro {
     constructor(nome) {
         this.nome = nome;
-        this.[_velocidade] = 0;
+        this. [_velocidade] = 0;
     }
 
     acelerar() {
-        if (this.[_velocidade] >= 100) return;
-        this.[_velocidade]++;
+        if (this[_velocidade] >= 100) return;
+        this[_velocidade]++;
     }
     freiar() {
-        if (this.[_velocidade] <= 0) return;
-        this.[_velocidade]--;
+        if (this[_velocidade] <= 0) return;
+        this[_velocidade]--;
     }
 };
 
@@ -20,7 +21,7 @@ const carro1 = new Carro('Fusca');
 
 //simulando se tivesse acelerando o fusca ate 200km por hra
 //ele nao pode passar de 100 pois ele esta setado a 100 por hra 
-for(let i =0; i <=200; i++){
+for (let i = 0; i <= 200; i++) {
     carro1.acelerar();
 };
 
