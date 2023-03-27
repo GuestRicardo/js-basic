a47ff1e9b89bf918ae9f42b13443bd5107e6f5f2
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-03-27 16:29:00 -0300

    formatando

--- 1/assets/js/exe28.js
+++ 1/assets/js/exe28.js
@@ -17,6 +17,7 @@ class Carro {
 const carro1 = new Carro('Fusca');
 
 //simulando se tivesse acelerando o fusca ate 200km por hra
+//ele nao pode passar de 100 pois ele esta setado a 100 por hra 
 for(let i =0; i <=200; i++){
     carro1.acelerar();
 };
