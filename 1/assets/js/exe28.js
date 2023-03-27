604a54990070d2c3e37ca766d19a0d00343dca61
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-03-27 16:38:07 -0300

    formatando

--- 1/assets/js/exe28.js
+++ 1/assets/js/exe28.js
@@ -24,5 +24,5 @@ const carro1 = new Carro('Fusca');
 for (let i = 0; i <= 200; i++) {
     carro1.acelerar();
 };
-
+//para acessar a velocidade por fora do objeto, teria q se ultilizar o get
 console.log(carro1);
