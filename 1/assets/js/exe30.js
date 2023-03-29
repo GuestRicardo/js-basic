65a6523b561d8ca3a0a88145f7eba5f47a804c4a
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-03-29 16:07:16 -0300

    formatando

--- 1/assets/js/exe30.js
+++ 1/assets/js/exe30.js
@@ -8,12 +8,22 @@ class ValidaFormulario {
         this.senha = document.querySelector('#senha');
 
         this.eventos();
-        this.formulario.addEventListener('submit', envio =>{
-            this.
-        })
     }
-    eventos() {
 
+    eventos() {
+        this.formulario.addEventListener('submit', evento => {
+            this.handleSubmit(evento); // muito usado em react
+        });
+    }
+    //para evitar de enviar algo errado, ou campo vazio
+    handleSubmit(evento) {
+        evento.preventDefault();
+        const camposvalidos = this.todosCamposValidos();
+    }
+    todosCamposValidos() {
+        let valido = true; //aq esta baseando q tu esta valido , e se caso ocorra um erro sera tratado em outro lugar
     }
 }
+
+
 const valida = new ValidaFormulario();
