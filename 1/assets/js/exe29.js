55639e3397af9ef98b4b18bff7f2420ca59f7a1d
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-03-29 09:42:10 -0300

    formatando

--- 1/assets/js/exe29.js
+++ 1/assets/js/exe29.js
@@ -1,7 +1,15 @@
+//Lembrete:
+//classe é a mesma coisa de função construtora
 class ValidaCpf {
-    constructor(cpfEnviado){
-    
+    constructor(cpfEnviado) {
+        Object.defineProperty(this, 'cpfLimpo', {
+            //para proteger ou ocultar as propriedades
+            writable: false, //nao aceita escrever
+            enumerable: false, //nao vai aparecer quando der console.log(no caso das propriedades do sistema)
+            configurable: false, //nao vai aceitar nenhuma configuração
+            value: cpfEnviado.replace(/\D+/g, '') //expressao regular é pra remover qualquer coisa q nao seja numero
+        });
     }
 }
 
-const ValidaCpf = new ValidaCpf('70548445052');
+const ValidaCpf = new ValidaCpf('705.484.450-52');
