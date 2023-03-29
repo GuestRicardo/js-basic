5427e3e04c6841dc53416b985a7c82379e67c125
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-03-29 14:33:29 -0300

    formatando

--- 1/assets/js/exe30.js
+++ 1/assets/js/exe30.js
@@ -1,1 +1,16 @@
-class
+class ValidaFormulario {
+    constructor() {
+        this.formulario = document.querySelector('#formulario');
+        this.nome = document.querySelector('#nome');
+        this.sbrenome = document.querySelector('#sobrenome');
+        this.cpf = document.querySelector('#cpf');
+        this.usuario = document.querySelector('#usuario');
+        this.senha = document.querySelector('#senha');
+
+        this.eventos();
+    }
+    eventos() {
+
+    }
+}
+const valida = new ValidaFormulario();
