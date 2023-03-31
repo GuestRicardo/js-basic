8cd35d578d28534fe1959d95961b11a58de2309f
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-03-31 16:19:54 -0300

    formatando

--- 1/assets/js/exe30.js
+++ 1/assets/js/exe30.js
@@ -23,7 +23,7 @@ class ValidaFormulario {
     todosCamposValidos() {
         let valido = true; //aq esta baseando q tu esta valido , e se caso ocorra um erro sera tratado em outro lugar
 
-        for(let campo of this.formulario.querySelectorAll())
+        for(let campo of this.formulario.querySelectorAll('.validar'))
     }
 }
 
