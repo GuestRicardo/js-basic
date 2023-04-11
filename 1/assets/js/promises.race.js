d45524762a822aeb671cbfed0c4fa524b4f5e5b6
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-04-11 09:13:55 -0300

    formatando

--- 1/assets/js/promises.race.js
+++ 1/assets/js/promises.race.js
@@ -8,23 +8,29 @@ function aleatorios(min, max) {
 function esperaAi(mensagem, tempo){
   return new Promise((resolve, reject)=>{
     if(typeof mensagem !== 'string'){
-      reject('NÂO aceita valores numericos')
+      reject('NÂO aceita valores numericos');
+      return;
     }
+    // O método global setTimeout()define um cronômetro que executa
+    //  uma função ou trecho de código especificado assim que o cronômetro expira.
     setTimeout(()=>{
-        resolve(mensagem.toUpperCase() + '-passou pela promise');
+        resolve(mensagem.toUpperCase() + ' - passou pela promise');
+        return;
     }, tempo);
   });
 }
 
+//array q passara a promise, e ela retornará como um array
 const promecas = [
-  'primeiro valor',
+  'primeiro valor',//este nao passará na promise
   esperaAi('promeça 1', 1,2),
   esperaAi('promeça 2', 2,3),
   esperaAi('promeça 3', 3,4),
   esperaAi('promeça 4', 4,5),
-  'outro valor'
+  'outro valor' //este nao passara na promise
 ];
 
+//resouver a promise
 Promise.race(promecas)
 .then(valor =>{
   console.log(valor);
