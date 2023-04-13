35ba46be92ef128143ee053656036c91845ad542
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-04-13 10:22:57 -0300

    formatando

--- 1/assets/js/exe31.js
+++ 1/assets/js/exe31.js
@@ -19,6 +19,13 @@ function esperaAi(mensagem, tempo) {
   });
 }
 
+//aq acontece a promise
 esperaAi('FAse 1', aleatorios(1, 3))
-.then(valor => console.log(valor))
-
+  .then(valor => {
+    console.log(valor); 
+    return esperaAi('fase2', aleatorios()) //retornando a promise
+  })
+  .then(fase => {
+    console.log(fase);
+    return esperaAi('')
+  })
