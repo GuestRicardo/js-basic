cf9a1bb22c46782727c82d8505959d6b0513fe89
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-04-14 16:21:33 -0300

    formatando

--- 1/assets/js/ajax.js
+++ 1/assets/js/ajax.js
@@ -1,6 +1,12 @@
- const request = (objeto)=>{
-  const xhr = new XMLDocument();
-  //este é verbo http, ele tambem e chamdo de metodo(get) ele serve para pegar as coisas na web
-  xhr.open(objeto.method, objeto.url, true );
-  
+ const request = (objeto) => {
+   const xhr = new XMLDocument();
+   //este é verbo http, ele tambem e chamdo de metodo(get) ele serve para pegar as coisas na web
+   xhr.open(objeto.method, objeto.url, true);
+   //se fosse POST estaria no formulario abaixo
+   xhr.send(); //aq estaria enviando(como aq nao enviaremos, ele pode ser null ou deixar em branco)
+
+   //aq é para saber se a requisição foi carregada
+   xhr.addEventListener('load', (/**aq ficaria o evento, mas em nosso exemplo não vai precisar */)=>{
+    //quando chegar aq dentro ja tera a resposta
+   });
  }
