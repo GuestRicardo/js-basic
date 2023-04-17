f043c4b42fd35e9ea59fd52f1b42db8e3109a4f9
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-04-17 16:35:25 -0300

    formatando

--- 1/assets/js/fetchAPI.js
+++ 1/assets/js/fetchAPI.js
@@ -1,63 +1,48 @@
- const request = (objeto) => {
-   return new Promise((resolve, reject) => {
-     const xhr = new XMLHttpRequest();
-     xhr.open(objeto.method, objeto.url, true);
-     xhr.send();
+//  const request = (objeto) => {
+//    return new Promise((resolve, reject) => {
+//      const xhr = new XMLHttpRequest();
+//      xhr.open(objeto.method, objeto.url, true);
+//      xhr.send();
 
-     xhr.addEventListener('load', () => {
-       if (xhr.status >= 200 && xhr.status < 300) {
-         resolve(xhr.responseText);
-       } else {
-         reject(xhr.statusText);
-       }
-     });
-   });
- };
+//      xhr.addEventListener('load', () => {
+//        if (xhr.status >= 200 && xhr.status < 300) {
+//          resolve(xhr.responseText);
+//        } else {
+//          reject(xhr.statusText);
+//        }
+//      });
+//    });
+//  };
+/** FETCH API vai subistituir essa função */
 
- document.addEventListener('click', evento => {
-   const elemento = evento.target;
-   const tag = elemento.tagName.toLowerCase();
+document.addEventListener('click', evento => {
+  const elemento = evento.target;
+  const tag = elemento.tagName.toLowerCase();
 
-   if (tag == 'a') {
-     evento.preventDefault();
-     carregaPagina(elemento);
-   }
- });
+  if (tag == 'a') {
+    evento.preventDefault();
+    carregaPagina(elemento);
+  }
+});
 
- async function carregaPagina(elemento) {
+function carregaPagina(elemento) {
+  const href = elemento.getAttribute('href');
 
-   const href = elemento.getAttribute('href');
-   const objConfig = {
-     method: 'GET',
-     url: href
-   };
+  fetch(href)//carregando o link
+  .then(response => { //pegando a resposta
+    response.text() //convertendo a resposta em texto
+    if(response.status !== 200){
+      throw new Error('ERRO 404!')
+    }
+  }).then(html =>{ //retornando o texto em html
+    carregaResultado(html)
+  }) .catch(erro =>{
+    console.warn(erro)
+  })
+}
 
-   try {
-     const response = await request(objConfig);
-     carregaResultado(response);
-   } catch (erro) {
-     console.log('ERRO AO CARREGAR A PAGINA', erro);
-   }
- }
+function carregaResultado(response) {
+  const resultado = document.querySelector('.resultado');
+  resultado.innerHTML = response;
+}
 
- function carregaResultado(response) {
-   const resultado = document.querySelector('.resultado');
-   resultado.innerHTML = response;
- }
-
-
- //carregando o conteudo com javascript
- fetch('exercicio17.html') //ela ja retorna por padrao uma promise
-   .then(resposta => { //tratando promise 
-     if (resposta.status !== 200) {
-       throw new Error('ERRO 404 no throw');
-      }
-      return resposta.text();
-   })
-   //pegando o html em si
-   .then(html =>{
-    console.log(html);
-   })
-   .catch(erro => {
-      console.log(erro);
-   });
