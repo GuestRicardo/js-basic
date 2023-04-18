04062bcfc3a75e146e978773767ab29dd762a6f8
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-04-18 14:39:15 -0300

    formatando

--- 1/assets/js/fetchAPIeAXIOS.js
+++ 1/assets/js/fetchAPIeAXIOS.js
@@ -1,1 +1,3 @@
-fetch('../json/pessoas.json')
+fetch('../json/pessoas.json') //carregando o json
+  .then(resposta => resposta.json()) //capturando os dados
+  .then(json => console.log(json)); //carregando as infos no navegador
