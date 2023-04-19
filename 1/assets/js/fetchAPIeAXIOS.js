4ccc1284c1f3396d287872b5a4b6495e663b71e6
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-04-19 14:52:49 -0300

    formatando

--- 1/assets/js/fetchAPIeAXIOS.js
+++ 1/assets/js/fetchAPIeAXIOS.js
@@ -7,27 +7,23 @@ function carregaElementosNaPagina(json) {
   console.log(json) //teste pra saber se carregou o json
 
   for (let pessoa of json) {
-    let tr = document.createElement('tr');
+    const tr = document.createElement('tr');
 
     let coluna1 = document.createElement('td');
     coluna1.innerHTML = pessoa.nome;
     tr.appendChild(coluna1);
 
-    // let coluna2 = document.createElement('td');
-    // coluna2.innerHTML = pessoa.sobrenome;
-    // tr.appendChild(coluna2);
+    let coluna3 = document.createElement('td','-');
+    coluna3.innerHTML = pessoa.idade;
+    tr.appendChild(coluna3);
 
-    // let coluna3 = document.createElement('td');
-    // coluna3.innerHTML = pessoa.idade;
-    // tr.appendChild(coluna3);
-
-    // let coluna4 = document.createElement('td');
-    // coluna4.innerHTML = pessoa.salario;
-    // tr.appendChild(coluna4);
+    let coluna4 = document.createElement('td');
+    coluna4.innerHTML = pessoa.salario;
+    tr.appendChild(coluna4);
 
     tabela.appendChild(tr);
 
   }
   const resultado = document.querySelector('.resultados');
-  resultado.apply(tabela);
+  resultado.appendChild(tabela);
 }
