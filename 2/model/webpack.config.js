8ba9128ed27414c18d0a73d7dc8e3ab7a7d92fad
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-04-25 16:44:54 -0300

    formatando

--- 2/model/webpack.config.js
+++ 2/model/webpack.config.js
@@ -2,4 +2,8 @@ const patch = require('path');
 
 module.exports = {
     mode: 'development',
+    entry: './src/index.js',
+    output: {
+        patch: path.resolve(__dirname, 'public', 'assets', 'js')
+    }
 };
