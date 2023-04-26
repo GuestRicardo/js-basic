25e4b6491c7092ae7eab3af9d7640974786b4186
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-04-26 16:26:38 -0300

    formatando

--- 2/model/webpack.config.js
+++ 2/model/webpack.config.js
@@ -1,11 +1,11 @@
-const patch = require('path');
+const path = require('path');
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
         patch: path.resolve(__dirname, 'public', 'assets', 'js'),
-        filename: 'bundle.js',
+        filename: 'bundle.js'
     },
     module: {
         rules:[{
