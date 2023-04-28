8964ef31089b08d1b70400bd9b4a455741b88a3f
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-04-28 15:39:24 -0300

    formatando

---
+++ 2/exercicios/exe1/models/modulo1.js
@@ -0,0 +1,9 @@
+const nome = 'Maria';
+const sobrenome = 'Jose';
+const idade = 89;
+
+function soma(x, y){
+    return x + y;
+}
+
+export { nome, sobrenome, idade, soma };
--- 2/exercicios/exe1/src/index.js
+++ 2/exercicios/exe1/src/index.js
@@ -1,4 +1,4 @@
-// 1
-// 2
-// 3
-console.log('EIIIIIIIIIIIIIIIIIIIIIII');
+import {nome, sobrenome, idade, soma} from '../models/modulo1';
+
+console.log(nome,' ',sobrenome,'Idade:', idade);
+console.log(`A soma da função importada é `, soma(5, 5));
