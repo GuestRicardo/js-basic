c9d31d94fea5b6be51de76ee4e6fdbc95bb6363f
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-04-28 15:47:54 -0300

    formatando

--- 2/exercicios/exe1/models/modulo1.js
+++ 2/exercicios/exe1/models/modulo1.js
@@ -6,4 +6,5 @@ function soma(x, y){
     return x + y;
 }
 
-export { nome, sobrenome, idade, soma };
+export { nome as nome2, sobrenome, idade, soma };
+//aq esta sendo exportado a variavel com outro nome, no caso o nome
--- 2/exercicios/exe1/src/index.js
+++ 2/exercicios/exe1/src/index.js
@@ -1,5 +1,5 @@
 import {
-    nome as nome2,
+    nome2 as nome3,
     sobrenome,
     idade,
     soma
@@ -8,5 +8,5 @@ import {
 //para caso estiver importando uma variavel q ja tenha o mesmo nome, é so usar o as
 const nome = 'Renato';
 
-console.log(nome2, '', sobrenome, ', Idade:', idade);
+console.log(nome3, '', sobrenome, ', Idade:', idade);
 console.log(`A soma da função importada é `, soma(5, 5));
