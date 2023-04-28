87d61936fecdb3da45a9566f112f8ea54c1e2fe7
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-04-28 15:45:27 -0300

    formatando

--- 2/exercicios/exe1/src/index.js
+++ 2/exercicios/exe1/src/index.js
@@ -1,4 +1,12 @@
-import {nome, sobrenome, idade, soma} from '../models/modulo1';
+import {
+    nome as nome2,
+    sobrenome,
+    idade,
+    soma
+} from '../models/modulo1';
 
-console.log(nome,'',sobrenome,', Idade:', idade);
+//para caso estiver importando uma variavel q ja tenha o mesmo nome, é so usar o as
+const nome = 'Renato';
+
+console.log(nome2, '', sobrenome, ', Idade:', idade);
 console.log(`A soma da função importada é `, soma(5, 5));
