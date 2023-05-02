94a7b99d2ac293bdbbd28eea32b007a1d78c4bc4
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-05-02 15:46:09 -0300

    formatando

--- 2/exercicios/exe1/src/index.js
+++ 2/exercicios/exe1/src/index.js
@@ -10,6 +10,6 @@ import {
 const nome = 'Renato';
 const p1 = new Pessoa('Jose', 'Maria');
 
+console.log(p1);
 console.log(nome3, '', sobrenome, ', Idade:', idade);
 console.log(`A soma da função importada é `, soma(5, 5));
-console.log(p1);
