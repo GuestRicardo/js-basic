e9023da2ed14d938afdb07c1d2680fcd848608f6
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-05-02 15:44:38 -0300

    formatando

--- 2/exercicios/exe1/src/index.js
+++ 2/exercicios/exe1/src/index.js
@@ -8,8 +8,8 @@ import {
 
 //para caso estiver importando uma variavel q ja tenha o mesmo nome, é so usar o as
 const nome = 'Renato';
+const p1 = new Pessoa('Jose', 'Maria');
 
 console.log(nome3, '', sobrenome, ', Idade:', idade);
 console.log(`A soma da função importada é `, soma(5, 5));
-
-const p1 = new Pessoa('Jose', 'Maria')
+console.log(p1);
