3094709eeba8d4412c64f589a4b780bf482baaf4
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-05-03 16:53:05 -0300

    formatando

--- 2/exercicios/geraCPF/src/main.js
+++ 2/exercicios/geraCPF/src/main.js
@@ -1,2 +1,10 @@
 import GeraCPF from './modules/geraCPF';
 import './assets/css/style.css';
+
+//função auto inicia
+(function(){
+    const gera = new GeraCPF();
+    const cpfGerado = document.querySelector('.cpf-gerado');
+    cpfGerado.innerHTML = gera.geraNovoCpf();
+
+})();
