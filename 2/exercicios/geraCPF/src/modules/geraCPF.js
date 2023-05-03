144e84083fd84575de3a5a2a3342c3b0ba1df8cf
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-05-03 16:47:51 -0300

    formatando

--- 2/exercicios/geraCPF/src/modules/geraCPF.js
+++ 2/exercicios/geraCPF/src/modules/geraCPF.js
@@ -4,5 +4,11 @@ export default class GeraCPF {
     rand(min = 100000000, max = 999999999) {
         return String(Math.floor(Math.random() * (max - min) + min));
     }
-    
+    geraNovoCpf(){
+        const cpfSemDigito = this.rand();
+        const digito1 = ValidaCPF.geraDigito(cpfSemDigito);
+        const digito2 = ValidaCPF.geraDigito(cpfSemDigito + digito1);
+        const novoCPF = cpfSemDigito + digito1 +digito2;
+        return novoCPF;
+    }
 }
