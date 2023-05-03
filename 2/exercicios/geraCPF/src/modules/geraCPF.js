2f81aa89e29e2a9e5c77e0f518a0bba24e6109b6
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-05-03 16:44:49 -0300

    formatando

--- 2/exercicios/geraCPF/src/modules/geraCPF.js
+++ 2/exercicios/geraCPF/src/modules/geraCPF.js
@@ -1,7 +1,8 @@
 import ValidaCPF from "./validaCPF";
 
-export default class GeraCPF{
-    constructor(){
-
+export default class GeraCPF {
+    rand(min = 100000000, max = 999999999) {
+        return String(Math.floor(Math.random() * (max - min) + min));
     }
+    
 }
