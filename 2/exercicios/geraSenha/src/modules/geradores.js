e9867c28200ed56db7561b58c1a6e132cbd8c84f
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-05-05 14:42:39 -0300

    ormatando

--- 2/exercicios/geraSenha/src/modules/geradores.js
+++ 2/exercicios/geraSenha/src/modules/geradores.js
@@ -2,5 +2,7 @@
 const rand = (min, max) => Math.floor(Math.random() * (max -min) + min);
 //gerando letras maiusculas
 const geraMaiuscula = () => String.fromCharCode(rand(65, 91));
+const geraMinuscula = () => String.fromCharCode(rand(97, 123));
 
 console.log(geraMaiuscula())
+console.log(geraMinuscula())
