d7a8ed2ebecf49826ea0d20eec2c2571686e5ea3
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-05-05 14:46:27 -0300

    formatando

--- 2/exercicios/geraSenha/src/modules/geradores.js
+++ 2/exercicios/geraSenha/src/modules/geradores.js
@@ -3,6 +3,9 @@ const rand = (min, max) => Math.floor(Math.random() * (max -min) + min);
 //gerando letras maiusculas
 const geraMaiuscula = () => String.fromCharCode(rand(65, 91));
 const geraMinuscula = () => String.fromCharCode(rand(97, 123));
+const geraNumeros = () => String.fromCharCode(rand(48, 58));
+const simbolos = ',.;/:?]}[{-_=+)(*&¨%$#@!"\|';
 
 console.log(geraMaiuscula())
 console.log(geraMinuscula())
+console.log(geraNumeros())
