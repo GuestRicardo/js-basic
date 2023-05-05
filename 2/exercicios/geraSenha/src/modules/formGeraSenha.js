3342634154e8f366498dc3a484dc8cb7cc49239f
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-05-05 16:40:05 -0300

    formatando

--- 2/exercicios/geraSenha/src/modules/formGeraSenha.js
+++ 2/exercicios/geraSenha/src/modules/formGeraSenha.js
@@ -1,5 +1,27 @@
 import geraSenha from "./geradores";
 
+const senhaGerada = document.querySelector('.senhaGerada');
+const  qtdCaracteres = document.querySelector('.qtdCaracteres');
+const  chkMaiusculas = document.querySelector('.chkMaiusculas');
+const  chkMinusculas = document.querySelector('.chkMinusculas');
+const  chkNumeros = document.querySelector('.chkNumeros');
+const  chkSimbolos = document.querySelector('.chkSimbolos');
+const  gerarSenha = document.querySelector('#gerarSenha');
+
 export default () =>{
-    console.log('teste')
+    gerarSenha.addEventListener('click', ()=>{
+       senhaGerada.innerHTML = gera(); 
+    })
+};
+
+function gera(){
+    const senha = geraSenha(
+        qtdCaracteres.value,
+        chkMaiusculas.checked,
+        chkMinusculas.checked,
+        chkNumeros.checked,
+        chkSimbolos.checked
+    );
+
+    return senha || 'Nada selecionado.';
 }
