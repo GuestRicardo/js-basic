cf66b49da545f619161fe5e4e47e7ec028f0845a
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-05-19 16:40:01 -0300

    formatando

---
+++ 3/exercicios/escrevendo-e-lendo/modules/escrever.js
@@ -0,0 +1,7 @@
+const fs = require('fs').promises;
+const path = require('path');
+const caminhoArquivo = path.resolve(__dirname, '..', 'test.txt');
+
+fs.writeFile(caminhoArquivo, 'Frase 1', {flag: 'w', encoding: 'utf-8'});
+
+
