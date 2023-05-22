a2acdecacc4bf51e49d7b3c432b67c541dce5ac5
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-05-22 15:27:21 -0300

    formatando

---
+++ 3/exercicios/escrevendo-e-lendo/app.js
@@ -0,0 +1,8 @@
+const pessoas =[
+    {nome: 'joao'},
+    {nome: 'maria'},
+    {nome: 'jose'},
+    {nome: 'eduardo'},
+    {nome: 'joaquim'},
+];
+const json = JSON.stringify(pessoas, '', 2);
--- 3/exercicios/escrevendo-e-lendo/modules/escrever.js
+++ 3/exercicios/escrevendo-e-lendo/modules/escrever.js
@@ -2,14 +2,7 @@ const fs = require('fs').promises;
 const path = require('path');
 const caminhoArquivo = path.resolve(__dirname, '..', 'test.json');
 
-const pessoas =[
-    {nome: 'joao'},
-    {nome: 'maria'},
-    {nome: 'jose'},
-    {nome: 'eduardo'},
-    {nome: 'joaquim'},
-];
-const json = JSON.stringify(pessoas, '', 2);
+
 
 fs.writeFile(caminhoArquivo, json, {flag: 'w', encoding: 'utf8'});
 
