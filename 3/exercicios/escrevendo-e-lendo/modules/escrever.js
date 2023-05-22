b6f76a875f4a32d9eeaf3faf263c17c808e722ad
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-05-22 16:05:44 -0300

    formatando

--- 3/exercicios/escrevendo-e-lendo/modules/escrever.js
+++ 3/exercicios/escrevendo-e-lendo/modules/escrever.js
@@ -1,7 +1,7 @@
 const fs = require('fs').promises;
 
-module.exports = (caminho)=> {
-    fs.readFile(caminho, 'utf8');
+module.exports = (caminho, dados)=> {
+    fs.writeFile(caminho, dados, {flag: 'w'});
 };
 
 
