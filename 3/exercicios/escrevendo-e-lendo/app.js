8b457c47f2ee6cf36d23618793e9cd34b3d35520
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-05-22 16:11:49 -0300

    formatando

--- 3/exercicios/escrevendo-e-lendo/app.js
+++ 3/exercicios/escrevendo-e-lendo/app.js
@@ -3,15 +3,15 @@ const caminhoArquivo = path.resolve(__dirname, 'test.json');
 const escrever = require('./modules/escrever');
 const ler = require('./modules/ler');
 
-// const pessoas =[
-//     {nome: 'joao'},
-//     {nome: 'maria'},
-//     {nome: 'jose'},
-//     {nome: 'eduardo'},
-//     {nome: 'joaquim'},
-// ];
-// const json = JSON.stringify(pessoas, '', 2);
-// escrever(caminhoArquivo, json);
+const pessoas =[
+    {nome: 'joao'},
+    {nome: 'maria'},
+    {nome: 'jose'},
+    {nome: 'eduardo'},
+    {nome: 'joaquim'},
+];
+const json = JSON.stringify(pessoas, '', 2);
+escrever(caminhoArquivo, json);
 
 async function lerArquivo(caminho) {
     const dados = await ler(caminho);
