43813dcae1eb5234bdbfe4e3a47075c770f0d035
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-05-23 09:17:35 -0300

    formatando

--- 3/exercicios/express-server/server.js
+++ 3/exercicios/express-server/server.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const app = express();
 
-app.get('/',(req, res)=>{
-    res.send(
+app.get('/', (req, res) => {
+    res.send(`
         <form action='/' method='nome'>
          Nome: <input type='text' name='nome'></input>
          <button>Enviar</button>
         </form>
-    );
+        `);
 });
-app.post('/',(req, res)=>{
+app.post('/', (req, res) => {
     res.send('Recebi o formulario');
 });
-app.get('/contato',(req, res)=>{
+app.get('/contato', (req, res) => {
     res.send('Obrigado por entrar em contato com agente');
 });
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log('Acesse o http://localhost:3000');
     console.log('Servidor executando com sucesso...');
 });
