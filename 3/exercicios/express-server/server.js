c4fba606e20f26d50d52a640a775c905bb9a2dc6
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-05-22 22:20:00 -0300

    formatanbdo

--- 3/exercicios/express-server/server.js
+++ 3/exercicios/express-server/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get('/',(res, req)=>{
+app.get('/',(req, res)=>{
     res.send('testando o server');
 });
 
-app.listen(3000,()=>{
+app.listen(3000, ()=>{
     console.log('Acesse o http://localhost:3000');
-    console.log('Servidor executando com sucesso...')
+    console.log('Servidor executando com sucesso...');
 });
