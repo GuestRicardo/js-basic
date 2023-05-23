5c76e1d23b29ada6ebfe7cac2647d2102f7d70b2
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-05-22 22:26:22 -0300

    formatando

--- 3/exercicios/express-server/server.js
+++ 3/exercicios/express-server/server.js
@@ -4,6 +4,9 @@ const app = express();
 app.get('/',(req, res)=>{
     res.send('testando o server');
 });
+app.get('/contato',(req, res)=>{
+    res.send('Obrigado por entrar em contato com agente');
+});
 
 app.listen(3000, ()=>{
     console.log('Acesse o http://localhost:3000');
