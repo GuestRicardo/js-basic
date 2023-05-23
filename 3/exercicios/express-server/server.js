13af2882e672f68f6434ccf1895451575f07a83d
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-05-22 22:17:39 -0300

    formatando

--- 3/exercicios/express-server/server.js
+++ 3/exercicios/express-server/server.js
@@ -5,4 +5,7 @@ app.get('/',(res, req)=>{
     res.send('testando o server');
 });
 
-app.listen(3000);
+app.listen(3000,()=>{
+    console.log('Acesse o http://localhost:3000');
+    console.log('Servidor executando com sucesso...')
+});
