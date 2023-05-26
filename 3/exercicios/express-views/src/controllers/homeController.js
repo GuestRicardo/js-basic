dd365ad76fa64846127014e1d41b99f6ad3ef080
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-05-26 16:07:15 -0300

    formatando

--- 3/exercicios/express-views/src/controllers/homeController.js
+++ 3/exercicios/express-views/src/controllers/homeController.js
@@ -1,11 +1,5 @@
 exports.paginaInicial = (req, res) => {
-    res.send(`
-        <form action='/' method='POST'>
-        Nome do cliente: <input type='text' name='qualquerCoisa'><br>
-        Outro campo qualquer: <input type='text' name='qualqueroutrocampo'><br>
-        <button>Enviar</button>
-        </form>
-    `)
+    res.render('index');
 };
 
 exports.trataPost = (req, res) => {
