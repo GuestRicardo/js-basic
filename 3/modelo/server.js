917d8a73cc94173605919e080a56991a6bd648f0
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-05-31 15:30:40 -0300

    formatando

--- 3/modelo/server.js
+++ 3/modelo/server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+//chave de conxao
+const connectString = 'mongodb+srv://gestricardo:ricardo22@cluster0.dsh0hpx.mongodb.net/teste?retryWrites=true&w=majority'
+//sinal emitido para poder escutar o db
+//conectando o db
+mongoose.connect(connectString);
+
 const routes = require('./routes');
 const path = require('path');
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({
+    extended: true
+}));
 
-app.use(express.static(path.resolve(__dirname,'public')));
+app.use(express.static(path.resolve(__dirname, 'public')));
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
