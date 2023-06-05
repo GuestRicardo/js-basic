3bbd2aeb4199fe76891917fac8299b817d97f443
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-06-05 14:54:32 -0300

    primeiro dado add no db

--- 3/modelo/src/controllers/homeController.js
+++ 3/modelo/src/controllers/homeController.js
@@ -1,7 +1,13 @@
 const HomeModel = require('../models/homeModel');
 
-//criando elemento q sera add no DB
-HomeModel.create();
+//criando elemento q sera add no DB, ele retornará uma promise, e retornará com elemento completo, com ids e tudo
+HomeModel.create({
+        titulo: 'Titulo para o primeiro teste',
+        descricao: 'Descrição para o primeiro teste'
+    })
+    //capturando elemento criado
+    .then(dados => console.log(dados))
+    .catch(erro => console.log(erro));
 
 exports.paginaInicial = (req, res) => {
     res.render('index');
