c78c57111245d30b05c06ec6bd63a8f29da4a629
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-06-12 13:59:53 -0300

    formatando

---
+++ 3/modelo/src/middlewares/middlewares.js
@@ -0,0 +1,14 @@
+exports.middlewareGlobal = (req, res, next) => {
+    res.locals.umaVariavelLocal = 'Este é o valor da variabel local';
+    next();
+};
+
+exports.outroMidlleware = (req, res, next)=>{
+    next();
+};
+
+exports.checkCSRFerror=(erro, req, res, next)=>{
+    if(erro && 'EBADCSRFTOKEN' === erro.code){
+        return res.semd('BAD CSRF') 
+    }
+}
