87216d4944c49bed776840204cd86b4c772546ac
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-06-19 15:30:09 -0300

    formatando

--- 3/exercicios/Agenda/routes.js
+++ 3/exercicios/Agenda/routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const route = express.Router();
 const homeController = require('./src/controllers/homeController');
-
+//lembrando q todas as rotas passara pelo index
 
 // Rotas da home
-route.get('/', homeController.index);
+route.get('/index', homeController.index);
+//rotas de login
+route.get('/login/index')
 
 module.exports = route;
