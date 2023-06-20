6f08b8e75f88ffc6ee4f54e47fa46651abec0575
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-06-20 10:50:34 -0300

    ndo

--- 3/exercicios/Agenda/src/controllers/loginController.js
+++ 3/exercicios/Agenda/src/controllers/loginController.js
@@ -1,3 +1,4 @@
+const Login = require('../model/LoginModel');
 
 exports.index = (req, res) => {
   res.render('login');
@@ -5,6 +6,8 @@ exports.index = (req, res) => {
 };
 //aq esta sendo pego o o q esta sendo enviado no form register
 exports.register = (req, res)=>{
+  //instancia da classe
+  const login = new Login(req.body);
   res.send(req.body);
 }
 
