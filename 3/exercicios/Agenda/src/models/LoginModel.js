98a3a6d815739195935c7b6275fb5e62984992fc
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-06-20 14:31:25 -0300

    formatando

--- 3/exercicios/Agenda/src/models/LoginModel.js
+++ 3/exercicios/Agenda/src/models/LoginModel.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 
 const LoginSchema = new mongoose.Schema({
-  titulo: {
+  email: {
     type: String,
     required: true
   },
-  descricao: String
+  senha: {
+    type: String,
+    required: true
+  },
 });
 
 const LoginModel = mongoose.model('Login', LoginSchema);
@@ -13,28 +16,34 @@ const LoginModel = mongoose.model('Login', LoginSchema);
 class Login {
   constructor(body) {
     this.body = body;
-    this.errors =[];
+    this.errors = [];
     this.user = null;
   }
 
-  register(){
+  register() {
     this.valida();
   }
 
-  valida(){
+  valida() {
     //validação
     //o email precisa ser valido
     //a senha precisa ter entre 8 a 10 caracteres
     this.cleanUp();
   }
 
-  cleanUp(){
+  cleanUp() {
     //for para garantir q tudo q esta no body seja string
-    for(const key in this.body){
-     if (typeof this.body[key] !== 'string'){
-      this.body[key] = '';
-     }
+    for (const key in this.body) {
+      if (typeof this.body[key] !== 'string') {
+        this.body[key] = '';
+      }
     }
+    //para garantir q tudo va para o bd
+    this.body = {
+      email: this.body.email,
+      password: this.body.password
+    };
+
   }
 }
 
