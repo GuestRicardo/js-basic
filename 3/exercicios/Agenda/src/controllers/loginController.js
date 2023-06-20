ebd7820386a993517a4e516f6b37fc24f961e72d
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-06-20 14:14:14 -0300

    formatando

--- 3/exercicios/Agenda/src/controllers/loginController.js
+++ 3/exercicios/Agenda/src/controllers/loginController.js
@@ -2,12 +2,12 @@ const Login = require('../model/LoginModel');
 
 exports.index = (req, res) => {
   res.render('login');
-  return;
 };
 //aq esta sendo pego o o q esta sendo enviado no form register
 exports.register = (req, res)=>{
   //instancia da classe
   const login = new Login(req.body);
-  res.send(req.body);
+  login.register();
+  res.send(login.body);
 }
 
--- 3/exercicios/Agenda/src/models/LoginModel.js
+++ 3/exercicios/Agenda/src/models/LoginModel.js
@@ -13,6 +13,24 @@ const LoginModel = mongoose.model('Login', LoginSchema);
 class Login {
   constructor(body) {
     this.body = body;
+    this.errors =[];
+    this.user = null;
+  }
+
+  register(){
+    this.valida();
+  }
+
+  valida(){
+    //validação
+    //o email precisa ser valido
+    //a senha precisa ter entre 8 a 10 caracteres
+    this.cleanUp();
+  }
+
+  cleanUp(){
+    //for para garantir q tudo q esta no body seja string
+    for(){}
   }
 }
 
