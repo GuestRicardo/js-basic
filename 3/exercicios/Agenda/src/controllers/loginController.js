8237688ad61c82a0d6aaef4758ac4863df3fd7f7
Ricardo Vieira dos Santos <78381215+GuestRicardo@users.noreply.github.com>
2023-06-21 15:49:37 -0300

    formatando

--- 3/exercicios/Agenda/src/controllers/loginController.js
+++ 3/exercicios/Agenda/src/controllers/loginController.js
@@ -4,14 +4,14 @@ exports.index = (req, res) => {
   res.render('login');
 };
 //aq esta sendo pego o o q esta sendo enviado no form register
-exports.register = async (req, res)=>{
+exports.register = async (req, res) => {
   //instancia da classe
   const login = new Login(req.body);
-   await login.register();
+  await login.register();
 
-  if(login.errors.length > 0){
+  if (login.errors.length > 0) {
     req.flash('errors', login.errors);
-    req.session.save(function(){
+    req.session.save(function () {
       return res.redirect('back');
     });
     return;
@@ -19,4 +19,3 @@ exports.register = async (req, res)=>{
 
   res.send(login.errors);
 }
-
