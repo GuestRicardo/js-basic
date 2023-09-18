1ae6d8e0ec6cb53ba3f6da3db7b710cebfa5e259
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-09-18 13:39:47 -0300

    formatando

--- 4/projeto/server.js
+++ 4/projeto/server.js
@@ -1,3 +1,8 @@
-import teste from './app';
 
-teste();
+import app from './app';
+//para que o express verifique e atualiza a aplicação(o miniserver)
+const port = 3001;
+app.listen(port, ()=>{
+  console.log(`Escutando na porta ${port}`);
+  console.log(`CTRL + Clique em http://localhost: ${port}`);
+});
