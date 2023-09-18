ed4c3345ba6f93786fed627988deb191d5e3ec49
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-09-18 10:28:51 -0300

    formatando

--- 4/projeto/src/routes/homeRoutes.js
+++ 4/projeto/src/routes/homeRoutes.js
@@ -1,4 +1,13 @@
-/** As duas formas validas de importação */
-import express from 'express';
-const router = new express.Router();
+/** As três formas validas de importação */
+import { Router } from 'express';
+const router = new Router();
+
+//import express from 'express';
+//const router = new express.Router();
 /**++++++++++++++++++++++++++++++++++ */
+
+//rota criada
+router.get('/',(req, res)=>{res.send('testando rota')});
+
+//exportando a rota
+export default router;
