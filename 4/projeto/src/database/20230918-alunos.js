548db064045161875787cad4955bb11140b737f0
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-09-19 15:01:19 -0300

    formatando

--- 4/projeto/src/database/20230918-alunos.js
+++ 4/projeto/src/database/20230918-alunos.js
@@ -2,9 +2,28 @@ import { Sequelize } from "sequelize";
 
 modules.exports ={
   up:(queryInterface, Sequelize) =>{
-    return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    return queryInterface.createTable('alunos', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      sobreNome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      }
+    });
   },
   down:(queryInterface)=>{
-    return queryInterface.dropTable('users');
+    return queryInterface.dropTable('alunos');
   }
 };
