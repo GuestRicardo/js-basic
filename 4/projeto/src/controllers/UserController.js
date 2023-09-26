95ce2eb8987fd88d9c36dc42643805c56ab3052a
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-09-26 15:07:51 -0300

    formatando

--- 4/projeto/src/controllers/UserController.js
+++ 4/projeto/src/controllers/UserController.js
@@ -1,15 +1,13 @@
 import Aluno from "../model/User";
 
 class UserController {
- async index(req, res){
-  const novoAluno = await Aluno.create({
-    nome: 'jose',
-    sobrenome: 'maria',
-    email: 'josemaria@hotmail.com',
-    idade: 45,
-    peso: 89,
-    altura: 1.78,
-  });
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: 'jose',
+      sobrenome: 'maria',
+      email: 'josemaria@hotmail.com',
+      password: '12345678'
+    });
     res.json(novoAluno);
   }
 }
