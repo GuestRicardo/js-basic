5d380163f3bf135971afae3f18bd715a56e452f8
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-10-02 16:09:07 -0300

    formatando

--- 4/projeto/src/routes/userRoutes.js
+++ 4/projeto/src/routes/userRoutes.js
@@ -10,7 +10,9 @@ import usercontroller from '../controllers/UserControllerController';
 //rota criada
 router.post('/', usercontroller.store);
 router.get('/', usercontroller.index);
-router.get('/', usercontroller.show);
+router.get('/:id', usercontroller.show);
+router.put('/:id', usercontroller.update);
+router.delete('/:id', usercontroller.delete);
 
 //exportando a rota
 export default router;
