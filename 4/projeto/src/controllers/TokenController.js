3527277ff7a4ab53047c8b36d69b7eb2a33afe1c
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-10-03 14:35:16 -0300

    formatando

--- 4/projeto/src/controllers/TokenController.js
+++ 4/projeto/src/controllers/TokenController.js
@@ -1,7 +1,13 @@
 import User from "../model/User";
 
 class TokenController {
- async store(req, res){
+  async store(req, res) {
+    const {
+      email = '', passsword = ''
+    } = req.body;
+
+    const user = await User.findOne();
+
     res.json("teste");
   }
 }
