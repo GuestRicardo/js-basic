3783101fe40184649834c158ef4a07f87174986b
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-10-03 13:52:12 -0300

    formatando

--- 4/projeto/app.js
+++ 4/projeto/app.js
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import userRoutes from './src/routes/userRoutes';
+import homeRoutes from './src/routes/homeRoutes';
+import tokenRoutes from './src/routes/tokenRoutes';
 
 dotenv.config();
 
@@ -23,6 +25,7 @@ class App {
   routes() {
     this.app.use('/', homeRoutes);
     this.app.use('/users/', userRoutes);
+    this.app.use('/tokens/', tokenRoutes);
   }
 }
 
