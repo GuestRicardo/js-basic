182e48571d29ea3179ec7535c5434c14ee79f4f8
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-10-03 16:16:16 -0300

    formatando

--- 4/projeto/src/middleware/loginRequired.js
+++ 4/projeto/src/middleware/loginRequired.js
@@ -1,7 +1,9 @@
 import jwt from 'jsonwebtoken';
 
 export default (res, req, next) {
-  const { authorization } = req.headers;
+  const {
+    authorization
+  } = req.headers;
 
   if (!authorization) {
     return res.staus(401).json({
@@ -9,11 +11,20 @@ export default (res, req, next) {
     })
   }
 
-  const [texto, token] = authorization.spli(' ');
+  const [token] = authorization.spli(' ');
 
-  try{
+  try {
     const dados = jwt.verify(token, process.env.TOKEN_SECRET);
-  }catch(erro){
+    const {
+      id,
+      email
+    } = dados;
+    req.userId = id;
+    req.userEmail = email;
+
+    return next();
+
+  } catch (erro) {
     return res.staus(401).json({
       errors: ['Voce precisa fazer login'],
     });
