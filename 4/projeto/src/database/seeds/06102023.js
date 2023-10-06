045bdd3a73658101d9e568cbbbb16134e36c3e97
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-10-06 16:02:45 -0300

    formatndo

--- 4/projeto/src/database/seeds/06102023.js
+++ 4/projeto/src/database/seeds/06102023.js
@@ -3,21 +3,21 @@ const bcryptjs = require('bcryptjs');
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('nomeDaTabela', [{
-      name: 'Joao',
+      nome: 'Joao',
       email: 'joao23@hotmail.com',
       password_hash: await bcryptjs.hash('123456789', 9),
       create_at: new Date(),
       updated_at: new Date(),
     },
     {
-      name: 'Maria',
+      nome: 'Maria',
       email: 'jssdsd23@hotmail.com',
       password_hash: await bcryptjs.hash('123456789', 9),
       create_at: new Date(),
       updated_at: new Date(),
     },
     {
-      name: 'Jo',
+      nome: 'Jo',
       email: 'jodd23@hotmail.com',
       password_hash: await bcryptjs.hash('123456789', 9),
       create_at: new Date(),
