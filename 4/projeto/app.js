b05c2a4ae4730583f367bb374db886d4eff95e34
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-10-06 21:04:33 -0300

    formtando

--- 4/projeto/app.js
+++ 4/projeto/app.js
@@ -3,6 +3,7 @@ import express from 'express';
 import userRoutes from './src/routes/userRoutes';
 import homeRoutes from './src/routes/homeRoutes';
 import tokenRoutes from './src/routes/tokenRoutes';
+import alunosRoutes from './src/routes/alunoRoutes';
 
 dotenv.config();
 
@@ -26,6 +27,7 @@ class App {
     this.app.use('/', homeRoutes);
     this.app.use('/users/', userRoutes);
     this.app.use('/tokens/', tokenRoutes);
+    this.app.use('/alunos/', alunosRoutes);
   }
 }
 
