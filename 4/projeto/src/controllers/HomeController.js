d13cd7bd0ccff831e970d7f6aa513816a5d5d612
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-10-06 21:06:51 -0300

    formatand

--- 4/projeto/src/controllers/HomeController.js
+++ 4/projeto/src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from "../model/Aluno";
-
 class HomeController {
  async index(req, res){
-  const novoAluno = await Aluno.create({
-    nome: 'jose',
-    sobrenome: 'maria',
-    email: 'josemaria@hotmail.com',
-    idade: 45,
-    peso: 89,
-    altura: 1.78,
-  });
-    res.json(novoAluno);
+    res.json('Index');
   }
 }
 
