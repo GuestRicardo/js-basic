25cbad1a6b86eb446c332569bf37d9042b42ce54
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-10-09 16:06:26 -0300

    formatando

--- 4/projeto/src/controllers/AlunoController.js
+++ 4/projeto/src/controllers/AlunoController.js
@@ -1,17 +1,14 @@
 import Aluno from "../model/Aluno";
 
 class AlunoController {
- async index(req, res){
-  const novoAluno = await Aluno.create({
-    nome: 'jose',
-    sobrenome: 'maria',
-    email: 'josemaria@hotmail.com',
-    idade: 45,
-    peso: 89,
-    altura: 1.78,
-  });
-    res.json(novoAluno);
-  }
+  async index(req, res) {
+    const alunos = await Aluno.findAll();
+  };
+  async store(req, res) {}
+  async show(req, res) {}
+  async delete(req, res) {}
+  async update(req, res) {}
 }
+
 
 export default new AlunoController();
