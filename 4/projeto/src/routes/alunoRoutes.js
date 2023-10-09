e73f63aae37595ed4bf6d4a02132c1faf2a09bcb
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-10-09 15:56:03 -0300

    formatando

--- 4/projeto/src/routes/alunoRoutes.js
+++ 4/projeto/src/routes/alunoRoutes.js
@@ -4,10 +4,10 @@ const router = new Router();
 import alunocontroller from '../controllers/AlunoController';
 //rota criada
 router.get('/', alunocontroller.index);
-router.get('/', alunocontroller.index);
-router.get('/', alunocontroller.index);
-router.get('/', alunocontroller.index);
-router.get('/', alunocontroller.index);
+router.post('/', alunocontroller.store);
+router.put('/', alunocontroller.update);
+router.get('/', alunocontroller.show);
+router.delete('/', alunocontroller.delete);
 
 //exportando a rota
 export default router;
