c70e2908b1e235da9a59afde08482a297f545548
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-10-09 13:42:03 -0300

    formatando

--- 4/projeto/src/routes/alunoRoutes.js
+++ 4/projeto/src/routes/alunoRoutes.js
@@ -1,12 +1,7 @@
-/** As três formas validas de importação */
+
 import { Router } from 'express';
 const router = new Router();
-
-//import express from 'express';
-//const router = new express.Router();
-/**++++++++++++++++++++++++++++++++++ */
 import homecontroller from '../controllers/HomeController';
-
 //rota criada
 router.get('/', homecontroller.index);
 
