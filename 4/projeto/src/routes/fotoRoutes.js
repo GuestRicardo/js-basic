007dd54938a64f14b1915db9c4c5b623770221e4
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-10-16 14:45:57 -0300

    fotoRoutes

---
+++ 4/projeto/src/routes/fotoRoutes.js
@@ -0,0 +1,11 @@
+
+import { Router } from 'express';
+const router = new Router();
+
+import homecontroller from '../controllers/HomeController';
+
+
+router.get('/', homecontroller.index);
+
+
+export default router;
