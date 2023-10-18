5569596e7e1df952fa2428ed15555c8bf1b11bc4
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-10-18 16:06:47 -0300

    formatando

--- 4/projeto/src/routes/fotoRoutes.js
+++ 4/projeto/src/routes/fotoRoutes.js
@@ -1,11 +1,12 @@
 
 import { Router } from 'express';
+import loginRequired from '../middleware/loginRequired'
 
 import fotocontroller from '../controllers/FotoController';
 
 const router = new Router();
 
-router.post('/', fotocontroller.store);
+router.post('/',loginRequired, fotocontroller.store);
 
 
 export default router;
