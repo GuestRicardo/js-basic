1f6c6d135c6739a0f0873aef7055e2e3f2c97fb5
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-10-18 16:26:16 -0300

    formatando

---
+++ 4/projeto/src/database/migrations/20231018-cria-tabela-de-fotos-do-usuario.js
@@ -0,0 +1,43 @@
+import {
+  Sequelize
+} from "sequelize";
+
+modules.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+  down: (queryInterface) => {
+    return queryInterface.dropTable('users');
+  }
+};
