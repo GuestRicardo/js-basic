8783aad6ac49c8bc968c11da543555c572b7ba9a
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-10-19 16:07:16 -0300

    formatando

--- 4/projeto/src/controllers/FotoController.js
+++ 4/projeto/src/controllers/FotoController.js
@@ -1,18 +1,22 @@
 import multer from 'multer';
 import multerConfig from '../config/multerConfig';
+import Foto from '../model/Foto';
 
 const upload = multer(multerConfig).single('foto');
 
 
 class FotoController {
-  async store(req, res) {
-    return upload(req, res, (error) => {
+   store(req, res) {
+    return upload(req, res, async (error) => {
       if (error) {
         return res.status(400).json({
           errors: [error.code],
         });
       }
-     return res.json(req.file);
+      const {originalname, filename} = req.file;
+      const foto = await Foto.create({ originalname, filename });
+
+     return res.json(foto);
     });
   }
 }
