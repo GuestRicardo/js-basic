f38692d31f840bd6a7744cc52a2d24d263a6e16e
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-10-19 14:33:38 -0300

    formatando

--- 4/projeto/src/database/index.js
+++ 4/projeto/src/database/index.js
@@ -2,10 +2,11 @@ import { Sequelize } from "sequelize";
 import databaseConfig from '../config/database';
 import Aluno from "../model/Aluno";
 import User from "../model/User";
-import foto from ''
+import Foto from "../model/Foto"
 
-const models = [Aluno, User];
+const models = [Aluno, User, Foto];
 
 const connection = new Sequelize(databaseConfig);
 
 models.forEach((model)=> model.init(connection));
+models.forEach((model)=> model.associate && model.associate(connection.models));
