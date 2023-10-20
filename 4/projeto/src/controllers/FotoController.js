c9cd9c5fd20701f655b8fb52261efa94a19d4d34
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-10-20 14:27:25 -0300

    formatando

--- 4/projeto/src/controllers/FotoController.js
+++ 4/projeto/src/controllers/FotoController.js
@@ -6,21 +6,32 @@ const upload = multer(multerConfig).single('foto');
 
 
 class FotoController {
-   store(req, res) {
+  store(req, res) {
     return upload(req, res, async (error) => {
       if (error) {
         return res.status(400).json({
           errors: [error.code],
         });
       }
-      try{//caso exista
-        const {originalname, filename} = req.file;
-        const { aluno_id } = req.body;
-        const foto = await Foto.create({ originalname, filename, aluno_id });
+      try { //caso aluno exista
+        const {
+          originalname,
+          filename
+        } = req.file;
+        const {
+          aluno_id
+        } = req.body;
+        const foto = await Foto.create({
+          originalname,
+          filename,
+          aluno_id
+        });
 
-       return res.json(foto);
-      } catch(e){//caso nao exista
-
+        return res.json(foto);
+      } catch (e) { //caso aluno nao exista
+        return res.status(400).json({
+          errors: ["Aluno nao existe"]
+        })
       }
     });
   }
